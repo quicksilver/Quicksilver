48eb27cef1e639e4d798d16f5949a972c422036e
tiennou7 <tiennou7@d44230c2-7321-0410-b0f2-3d6f2d243adf>
2007-12-15 16:16:35 +0000

    Cleanup and try to make this work.
    git-svn-id: https://blacktree-alchemy.googlecode.com/svn/trunk@72 d44230c2-7321-0410-b0f2-3d6f2d243adf

--- Quicksilver/Application/Code/QSAdvancedPrefPane.h
+++ Quicksilver/Application/Code/QSAdvancedPrefPane.h
@@ -9,22 +9,9 @@
 @interface QSAdvancedPrefPane : QSPreferencePane {
 	IBOutlet NSTableView *prefSetsTable;
 	IBOutlet NSArrayController *prefSetsController;
-	IBOutlet NSBox *settingsBox;
-	
-	
-	IBOutlet NSTextField * titleField;
-	IBOutlet NSTextField * pretextField;
-	IBOutlet NSSlider * valueSlider;
-	IBOutlet NSPopUpButton * valuePopUp;
-	IBOutlet NSButton * valueSwitch;
-	IBOutlet NSTextField * valueField;
-	IBOutlet NSTextField * posttextField;
-	IBOutlet NSTextField * descriptionField;
 	
 	NSMutableDictionary *currentInfo;
 }
-- (IBAction)setValue:(id)sender;
 - (NSMutableDictionary *)currentInfo;
 - (void)setCurrentInfo:(NSMutableDictionary *)newCurrentInfo;
-- (void)refreshView;
 @end
