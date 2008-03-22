8b7b3943bce4d6e8f3358dbdbae032ac69e912f0
tiennou7 <tiennou7@d44230c2-7321-0410-b0f2-3d6f2d243adf>
2008-03-22 11:32:15 +0000

    Forgot those two.
    git-svn-id: https://blacktree-alchemy.googlecode.com/svn/trunk@105 d44230c2-7321-0410-b0f2-3d6f2d243adf

---
+++ Crucible/Code/QSCategories.h
@@ -0,0 +1,55 @@
+/*
+ *  QSCategories.h
+ *  Crucible
+ *
+ *  Created by Etienne on 09/01/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#import "QSExternals.h"
+#import "NSApplication+ServicesModification.h"
+#import "CGSPrivate+QSMods.h"
+#import "InertButton.h"
+#import "NDAlias+QSMods.h"
+#import "NDHotKeyEvent_QSMods.h"
+#import "NDProcess+QSMods.h"
+#import "NSAppleEventDescriptor_QSMods.h"
+#import "NSAppleEventDescriptor+QSTranslation.h"
+#import "NSAppleScript+QSStorage.h"
+#import "NSAppleScript+QSSubroutine.h"
+#import "NSAppleScript+QSSubroutineDiscovery.h"
+
+#import "BLTRResizeView.h"
+
+#import "NSApplication_BLTRExtensions.h"
+#import "NSArray_BLTRExtensions.h"
+#import "NSBezierPath_BLTRExtensions.h"
+#import "NSBundle_BLTRExtensions.h"
+#import "NSData_RangeExtensions.h"
+#import "NSDictionary+BLTRExtensions.h"
+#import "NSEvent+BLTRExtensions.h"
+#import "NSFileManager_BLTRExtensions.h"
+#import "NSGeometry_BLTRExtensions.h"
+#import "NSImage_BLTRExtensions.h"
+#import "NSMetadataItem+BLTRExtensions.h"
+#import "NSObject+BLTRExtensions.h"
+#import "NSPasteboard_BLTRExtensions.h"
+#import "NSScreen_BLTRExtensions.h"
+#import "NSSortDescriptor+BLTRExtensions.h"
+#import "NSStatusItem_BLTRExtensions.h"
+#import "NSString_BLTRExtensions.h"
+#import "NSTableView_BLTRExtensions.h"
+#import "NSTask+BLTRExtensions.h"
+#import "NSURL_BLTRExtensions.h"
+#import "NSUserDefaults_BLTRExtensions.h"
+#import "NSView_BLTRExtensions.h"
+#import "NSWindow_BLTRExtensions.h"
+#import "NSWorkspace_BLTRExtensions.h"
+
+#import "NSException_TraceExtensions.h"
+#import "NSColor_QSModifications.h"
+#import "NSCursor_InformExtensions.h"
+#import "NSIndexSet+Extensions.h"
+#import "NSObject+ReaperExtensions.h"
+#import "NSString_Purification.h"
---
+++ Crucible/External/QSExternals.h
@@ -0,0 +1,24 @@
+/*
+ *  QSExternals.h
+ *  Crucible
+ *
+ *  Created by Etienne on 09/01/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#import "NDExtensions.h"
+
+#import "UKKQueue.h"
+#import "UKFNSubscribeFileWatcher.h"
+#import "UKMainThreadProxy.h"
+#import "UKDirectoryEnumerator.h"
+
+#import "NSImage+QuickLook.h"
+
+#import "CGSPrivate.h"
+
+#import "CPSPrivate.h"
+
+#import "QLPrivate.h"
+#import "QLUIPrivate.h"
