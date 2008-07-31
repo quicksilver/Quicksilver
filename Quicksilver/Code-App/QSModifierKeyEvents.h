40d844aed315436c6cb8d3ab7b26b7b3435db1ba
tiennou7 <tiennou7@d44230c2-7321-0410-b0f2-3d6f2d243adf>
2008-07-31 20:25:43 +0000

    r928@myBook:  tiennou | 2008-07-31 22:11:56 +0200  Reworked this to make it work when QS didn't have the focus.  The creepy reason is that we are transforming Carbon events into Cocoa ones, but the rest of the code (which expects our app runloop to be running) can't work because of this.  Removed some old GetKeys code (we already know which modifier has been pressed, why recheck ? ;-)).  Also, QS is now able to handle more than Simple/Double keystrokes, and Caps Lock is working in both Simple/Double mode now. I guess the GUI needs an update.
    git-svn-id: https://blacktree-alchemy.googlecode.com/svn/branches/B5X@218 d44230c2-7321-0410-b0f2-3d6f2d243adf

--- Quicksilver/Code-App/QSModifierKeyEvents.h
+++ Quicksilver/Code-App/QSModifierKeyEvents.h
@@ -10,7 +10,6 @@
 
 extern int NSAllModifierKeysMask;
 
-unsigned int lastModifiers;
 @interface NSApplication (QSModifierKeyEvents)
 - (BOOL)checkForModifierEvent:(NSEvent *)theEvent;
 @end
@@ -18,29 +17,31 @@ unsigned int lastModifiers;
 @interface QSModifierKeyEvent : NSObject {
 	unsigned int modifierActivationMask;
 	int modifierActivationCount;
-	NSString *identifier;
+	
+    NSString *identifier;
+    NSMutableArray * /* NSDate * */ activationAttempts;
 	id target;
 	SEL action;
 
-
-	@private
-		int keyCode;
+/*	@private
+		int keyCode;*/
 }
 + (BOOL)checkForModifierEvent:(NSEvent *)theEvent;
 + (QSModifierKeyEvent *)eventWithIdentifier:(NSString *)identifier;
 
+
 - (void)enable;
 - (void)disable;
-+(BOOL)alphaShiftReleased;
-- (BOOL)checkForModifierTap;
-+(BOOL)modifierToggled:(unsigned int)modifierKeysMask;
-- (unsigned int) modifierActivationMask;
+- (BOOL)modifierToggled:(unsigned int)modifierKeysMask countTimes:(unsigned int)count;
+//- (BOOL)checkForModifierTap;
+
+- (unsigned int)modifierActivationMask;
 - (void)setModifierActivationMask:(unsigned int)newModifierActivationMask;
-- (int) modifierActivationCount;
+- (int)modifierActivationCount;
 - (void)setModifierActivationCount:(int)newModifierActivationCount;
 - (id)target;
 - (void)setTarget:(id)newTarget;
-- (SEL) action;
+- (SEL)action;
 - (void)setAction:(SEL)newAction;
 - (NSString *)identifier;
 - (void)setIdentifier:(NSString *)newIdentifier;
