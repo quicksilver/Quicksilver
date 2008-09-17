629b1b3e5a21c32d9eddb6dae85dc19e4ab68ceb
tiennou7 <tiennou7@d44230c2-7321-0410-b0f2-3d6f2d243adf>
2008-09-17 10:06:04 +0000

    r934@myBook:  tiennou | 2008-08-06 20:42:55 +0200  Removed some duplicate code. Fixed a missing retain.
    git-svn-id: https://blacktree-alchemy.googlecode.com/svn/branches/B5X@225 d44230c2-7321-0410-b0f2-3d6f2d243adf

--- Quicksilver/Code-QuickStepCore/QSGlobalSelectionProvider.m
+++ Quicksilver/Code-QuickStepCore/QSGlobalSelectionProvider.m
@@ -1,129 +1,3 @@
-#if 0
-
-
-//
-// QSGlobalSelectionProvider.m
-// Quicksilver
-//
-// Created by Alcor on 1/21/05.
-// Copyright 2005 Blacktree. All rights reserved.
-//
-
-#import "QSGlobalSelectionProvider.h"
-
-#import "QSRegistry.h"
-
-#import "QSProxyObject.h"
-#import "QSObject_StringHandling.h"
-#import "QSObject_Pasteboard.h"
-
-#import "NDProcess.h"
-
-#import "NDProcess+QSMods.h"
-
-@interface QSTemporaryServiceProvider : NSObject {
-	NSPasteboard *resultPboard;
-}
-- (void)invokeService;
-- (NSPasteboard *)getSelectionFromFrontApp;
-@end
-
-@implementation QSTemporaryServiceProvider
-- (id)init {
-	if (self = [super init]) {
-		resultPboard = nil;
-	}
-	return self;
-}
-- (void)getSelection:(NSPasteboard *)pboard userData:(NSString *)userData error:(NSString **)error {
-	if (VERBOSE) NSLog(@"Get Selection: %@ %d", userData, [userData characterAtIndex:0]);
-	if(resultPboard)
-		[resultPboard release];
-	resultPboard = [pboard retain];
-}
-- (void)performService:(NSPasteboard *)pboard userData:(NSString *)userData error:(NSString **)error {
-	if (VERBOSE) NSLog(@"xPerform Service: %@ %d", userData, [userData characterAtIndex:0]);
-}
-- (NSPasteboard *)getSelectionFromFrontApp {
-	id oldServicesProvider = [NSApp servicesProvider];
-	[NSApp setServicesProvider:self];
-	[NSThread detachNewThreadSelector:@selector(invokeService) toTarget:self withObject:nil];
-	NSRunLoop *loop = [NSRunLoop currentRunLoop];
-	NSDate *date = [NSDate date];
-	while(!resultPboard && [date timeIntervalSinceNow] >-2) {
-		[loop runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.5]];
-	}
-	[NSApp setServicesProvider:oldServicesProvider];
-	id result = [resultPboard autorelease];
-	resultPboard = nil;
-	return result;
-}
-
-- (void)dealloc {
-	[resultPboard release];
-	[super dealloc];
-}
-
-- (void)invokeService {
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	pid_t pid = [[[[NSWorkspace sharedWorkspace] activeApplication] objectForKey:@"NSApplicationProcessIdentifier"] intValue];
-	AXUIElementRef app = AXUIElementCreateApplication (pid);
-
-	AXUIElementPostKeyboardEvent (app, (CGCharCode) 0, (CGKeyCode)55, true ); //Command
-//	if (carbon) AXUIElementPostKeyboardEvent (app, (CGCharCode) 0, (CGKeyCode)56, true ); //Shift
-	AXUIElementPostKeyboardEvent (app, (CGCharCode) 0, (CGKeyCode)53, true ); //Escape
-	AXUIElementPostKeyboardEvent (app, (CGCharCode) 0, (CGKeyCode)53, false ); //Escape
-//	if (carbon) AXUIElementPostKeyboardEvent (app, (CGCharCode) 0, (CGKeyCode)56, false ); //Shift
-	AXUIElementPostKeyboardEvent (app, (CGCharCode) 0, (CGKeyCode)55, true ); //Command
-	[pool release];
-}
-
-@end
-
-@implementation QSGlobalSelectionProvider
-
-NSTimeInterval failDate = 0;
-
-+(id)currentSelection {
-	NSDictionary *info = [[QSReg tableNamed:@"QSProxies"] objectForKey:[[[NSWorkspace sharedWorkspace] activeApplication] objectForKey:@"NSApplicationBundleIdentifier"]];
-	if (info) {
-		return [[QSReg getClassInstance:[info objectForKey:kQSProxyProviderClass]] resolveProxyObject:nil];
-	} else {
-		NSPasteboard *pb = nil;
-		if ([NSDate timeIntervalSinceReferenceDate] - failDate > 3.0){
-			QSTemporaryServiceProvider *sp = [[QSTemporaryServiceProvider alloc] init];
-			pb = [sp getSelectionFromFrontApp];
-			[sp release];
-		}
-		if (!pb) {
-			failDate = [NSDate timeIntervalSinceReferenceDate];
-			return nil;
-		}
-		return [QSObject objectWithPasteboard:pb];
-	}
-//	return [QSObject objectWithString:@"No Selection"]; //[QSObject nullObject];
-}
-
-- (id)resolveProxyObject:(id)proxy {
-	return [QSGlobalSelectionProvider currentSelection];
-}
-
-- (BOOL)bypassValidation {
-	return ([[[[NSWorkspace sharedWorkspace] activeApplication] objectForKey:@"NSApplicationBundleIdentifier"] isEqualToString:@"com.blacktree.Quicksilver"]);
-}
-- (NSArray *)typesForProxyObject:(id)proxy {
-	NSDictionary *info = [[QSReg tableNamed:@"QSProxies"] objectForKey:[[[NSWorkspace sharedWorkspace] activeApplication] objectForKey:@"NSApplicationBundleIdentifier"]];
-	if (!info) return [NSArray arrayWithObjects:NSStringPboardType, NSFilenamesPboardType, nil];
-	NSArray *array = [info objectForKey:kQSProxyTypes];
-	if (array) return array;
-	else return [[QSReg getClassInstance:[info objectForKey:kQSProxyProviderClass]] typesForProxyObject:self];
-}
-@end
-
-
-#else
-
-
 //
 // QSGlobalSelectionProvider.m
 // Quicksilver
@@ -159,19 +33,24 @@ NSTimeInterval failDate = 0;
 	}
 	return self;
 }
+
 - (void)getSelection:(NSPasteboard *)pboard userData:(NSString *)userData error:(NSString **)error {
 	if (VERBOSE) NSLog(@"Get Selection: %@ %d", userData, [userData characterAtIndex:0]);
+    if(resultPboard)
+        [resultPboard release]; 
 	resultPboard = [pboard retain];
 }
+
 - (void)performService:(NSPasteboard *)pboard
 			  userData:(NSString *)userData
 				 error:(NSString **)error {
 	if (VERBOSE) NSLog(@"xPerform Service: %@ %d", userData, [userData characterAtIndex:0]);
 }
+
 - (NSPasteboard *)getSelectionFromFrontApp {
 	
 	//NSLog(@"GET SEL");
-	id oldServicesProvider = [NSApp servicesProvider];
+	id oldServicesProvider = [[NSApp servicesProvider] retain];
 	[NSApp setServicesProvider:self];
 	//[self invokeService];
 	[NSThread detachNewThreadSelector:@selector(invokeService)
@@ -186,11 +65,11 @@ NSTimeInterval failDate = 0;
 	}
 	//	NSLog(@"got %@", resultPboard);
 	[NSApp setServicesProvider:oldServicesProvider];
+    [oldServicesProvider release];
 	id result = [resultPboard autorelease];
 	resultPboard = nil;
 	return result;
 }
-
 
 - (void)dealloc {
 	//	NSLog(@"release");
@@ -223,7 +102,7 @@ NSTimeInterval failDate = 0;
 
 NSTimeInterval failDate = 0;
 
-+(id)currentSelection {
++ (id)currentSelection {
 	NSDictionary *appDictionary = [[NSWorkspace sharedWorkspace] activeApplication];
 	//while(1);
 	NSString *identifier = [appDictionary objectForKey:@"NSApplicationBundleIdentifier"];
@@ -250,11 +129,13 @@ NSTimeInterval failDate = 0;
 	}
 	return [QSObject objectWithString:@"No Selection"]; //[QSObject nullObject];
 }
+
 - (id)resolveProxyObject:(id)proxy {
 	id object = [QSGlobalSelectionProvider currentSelection];
 	//NSLog(@"object %@", object);
 	return object;
 }
+
 - (BOOL)bypassValidation {
 	NSDictionary *appDictionary = [[NSWorkspace sharedWorkspace] activeApplication];
 	NSString *identifier = [appDictionary objectForKey:@"NSApplicationBundleIdentifier"];
@@ -263,6 +144,7 @@ NSTimeInterval failDate = 0;
 	else
 		return NO;
 }
+
 - (NSArray *)typesForProxyObject:(id)proxy {
 	NSDictionary *appDictionary = [[NSWorkspace sharedWorkspace] activeApplication];
 	NSString *identifier = [appDictionary objectForKey:@"NSApplicationBundleIdentifier"];
@@ -276,5 +158,3 @@ NSTimeInterval failDate = 0;
 }
 @end
 
-
-#endif
