b4751b25dd02de764aa2040c705ce1396fb3e388
tiennou7 <tiennou7@d44230c2-7321-0410-b0f2-3d6f2d243adf>
2009-06-10 07:06:55 +0000

    Tweaks to QSObject Dragging category
    - Added a protocol for related object handler.
    - Cleanup.
    
    git-svn-id: https://blacktree-alchemy.googlecode.com/svn/trunk@317 d44230c2-7321-0410-b0f2-3d6f2d243adf

--- Crucible/Code/QSObject_Drag.h
+++ Crucible/Code/QSObject_Drag.h
@@ -1,8 +1,16 @@
 
 
 #import <Foundation/Foundation.h>
-#import "QSObject.h"
-@interface QSObject (Drag)
+#import <QSCrucible/QSObject.h>
+
+@class QSBasicObject;
+
+@protocol QSObjectHandler_Dragging
+- (NSDragOperation)operationForDrag:(id <NSDraggingInfo>) ontoObject:(QSBasicObject *)destObject withObject:(QSBasicObject *)srcObject;
+- (NSString *)actionForDragMask:(NSDragOperation) ontoObject:(QSBasicObject *)destObject withObject:(QSBasicObject *)srcObject;
+@end
+
+@interface QSObject (Dragging)
 - (NSDragOperation)draggingEntered:(id <NSDraggingInfo>)sender withObject:(QSBasicObject *)object;
 - (NSString *)actionForDragOperation:(NSDragOperation)operation withObject:(QSBasicObject *)object;
 - (BOOL)performDragOperation:(id <NSDraggingInfo>)sender withObject:(QSBasicObject *)object;
--- Crucible/Code/QSObject_Drag.m
+++ Crucible/Code/QSObject_Drag.m
@@ -3,31 +3,29 @@
 #import "QSObject_Drag.h"
 #import "QSDebug.h"
 
-
 @implementation QSObject (Drag)
 
-- (NSDragOperation)draggingEntered:(id <NSDraggingInfo>)sender withObject:(QSBasicObject *)object{
-    NSDragOperation sourceDragMask=[sender draggingSourceOperationMask];
-    id handler=[self handler];
-    if ([handler respondsToSelector:@selector(operationForDrag:ontoObject:withObject:)])
+- (NSDragOperation)draggingEntered:(id <NSDraggingInfo>)sender withObject:(QSBasicObject *)object {
+    NSDragOperation sourceDragMask = [sender draggingSourceOperationMask];
+    
+    id handler = [self handlerForSelector:@selector(operationForDrag:ontoObject:withObject:)];
+    
+    if (handler)
         return [handler operationForDrag:sender ontoObject:self withObject:object];
     
-    //if (VERBOSE) QSLog(@"Unhandled drag");
-    return sourceDragMask&NSDragOperationGeneric;
+    return sourceDragMask & NSDragOperationGeneric;
 }
 
-
-- (NSString *)actionForDragOperation:(NSDragOperation)operation withObject:(QSBasicObject *)object{
-    id handler=[self handler];
-    if ([handler respondsToSelector:@selector(actionForDragMask:ontoObject:withObject:)])
+- (NSString *)actionForDragOperation:(NSDragOperation)operation withObject:(QSBasicObject *)object {
+    id handler = [self handlerForSelector:@selector(actionForDragMask:ontoObject:withObject:)];
+    
+    if (handler)
         return [handler actionForDragMask:operation ontoObject:self withObject:object];
+    
     return nil;
 }
 
-
-
-
-- (BOOL)performDragOperation:(id <NSDraggingInfo>)sender withObject:(QSBasicObject *)object{
+- (BOOL)performDragOperation:(id <NSDraggingInfo>)sender withObject:(QSBasicObject *)object {
     return YES;
 }
 
