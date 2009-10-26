8e2306e25bfae57e80eb84698ed41fcd67deeb0f
André Berg <andre.bergmedia@googlemail.com>
2009-10-26 05:10:42 +0100

    Add missing external references which weren't carried over from the svn conversion.

---
+++ Quicksilver/Code-External/NSImageQuickLook/NSImage+QuickLook.h
@@ -0,0 +1,17 @@
+//
+//  NSImage+QuickLook.h
+//  QuickLookTest
+//
+//  Created by Matt Gemmell on 29/10/2007.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSImage (QuickLook)
+
+
++ (NSImage *)imageWithPreviewOfFileAtPath:(NSString *)path ofSize:(NSSize)size asIcon:(BOOL)icon;
+
+
+@end
