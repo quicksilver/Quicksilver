23f389b7e1be800c83628b0b33bb8ff0c7de0d16
André Berg <andre.bergmedia@googlemail.com>
2009-11-11 08:26:50 +0800

    Ifdef out protocol defs not available on 10.5 (thanks Michael Guntsche).
    Should work towards a buildable QS on Mac OS X 10.5 ;)

--- Quicksilver/Code-QuickStepCore/QSObject_Menus.h
+++ Quicksilver/Code-QuickStepCore/QSObject_Menus.h
@@ -3,7 +3,10 @@
 #import <Foundation/Foundation.h>
 
 #import "QSObject.h"
-@interface QSObject (Menus) <NSMenuDelegate>
+@interface QSObject (Menus)
+#if (MAC_OS_X_VERSION_MAX_ALLOWED >= 1060)
+   <NSMenuDelegate>
+#endif
 - (NSMenu *)menu;
 - (NSMenuItem *)menuItem;
 - (NSMenu *)rankMenuWithTarget:(NSView *)target;
