0959cadc9987aef9fd4be33bc7ac1165cb43824d
neurolepsy <github@neurolepsy.com>
2011-03-13 02:12:52 +1100

    Correct type for bundles

--- Quicksilver/Code-QuickStepCore/QSObject_FileHandling.m
+++ Quicksilver/Code-QuickStepCore/QSObject_FileHandling.m
@@ -662,7 +662,7 @@ NSArray *recentDocumentsForBundle(NSString *bundleIdentifier) {
 
 	if (includeKind) {
         NSString *kind = nil;
-		if ([[path pathExtension] caseInsensitiveCompare:@"prefPane"]) {
+		if ([[path pathExtension] caseInsensitiveCompare:@"prefPane"] == NSOrderedSame) {
 			kind = QSGetLocalizationStatus() ? [self localizedPrefPaneKind] : @"Preference Pane";
 		} else {
 			LSCopyKindStringForURL((CFURLRef)fileURL, (CFStringRef *)&kind);
