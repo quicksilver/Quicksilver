561f190cecd6cae7a4965dd3b0e086dd4d3cace1
Rob McBroom <github@skurfer.com>
2011-04-11 16:17:28 -0400

    * renamed "Most Recent Download" proxy object to "Lastest Download" * It now ignores folders and downloads in progress * Moved new QSDownload class files in Xcode

--- Quicksilver/Code-QuickStepCore/QSDownloads.m
+++ Quicksilver/Code-QuickStepCore/QSDownloads.m
@@ -11,13 +11,26 @@
 - (id)resolveProxyObject:(id)proxy {
     NSString *downloads = [@"~/Downloads" stringByStandardizingPath];
     NSFileManager *manager = [NSFileManager defaultManager];
+    BOOL isDir;
     NSArray *contents = [manager contentsOfDirectoryAtPath:downloads error:nil];
     NSString *mrdpath;
     NSDate *mostRecent = [NSDate distantPast];
     for (NSString *downloadedFile in contents) {
-        if ([downloadedFile isEqualToString:@".DS_Store"]) continue;
-        if ([downloadedFile isEqualToString:@".localized"]) continue;
+        if (
+            // system files
+            [downloadedFile isEqualToString:@".DS_Store"] ||
+            [downloadedFile isEqualToString:@".localized"] ||
+            // Safari downloads in progress
+            [[downloadedFile pathExtension] isEqualToString:@".download"] ||
+            // Firefox downloads in progress
+            [[downloadedFile pathExtension] isEqualToString:@".part"] ||
+            [[downloadedFile pathExtension] isEqualToString:@".dtapart"] ||
+            // Chrome downloads in progress
+            [[downloadedFile pathExtension] isEqualToString:@".crdownload"]
+        ) continue;
         NSString *downloadPath = [downloads stringByAppendingPathComponent:downloadedFile];
+        // ignore folders
+        if ([manager fileExistsAtPath:downloadPath isDirectory:&isDir] && isDir) continue;
         NSDate *modified = [[manager attributesOfItemAtPath:downloadPath error:nil] fileModificationDate];
         if ([mostRecent compare:modified] == NSOrderedAscending) {
             mostRecent = modified;
