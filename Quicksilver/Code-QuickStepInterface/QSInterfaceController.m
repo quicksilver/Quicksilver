ac59ffd0300b776875ffe664b68396a035152e61
Rob McBroom <github@skurfer.com>
2011-04-26 16:46:07 -0400

    new method that allows the results list to be displayed with and without the first pane populated
    showArray and searchArray now use this method instead of one calling the other

--- Quicksilver/Code-QuickStepInterface/QSInterfaceController.m
+++ Quicksilver/Code-QuickStepInterface/QSInterfaceController.m
@@ -325,24 +325,34 @@
 }
 
 - (void)searchArray:(NSArray *)array {
-	[actionsUpdateTimer invalidate];
-	[self clearObjectView:dSelector];
-    NSMutableArray *mutArray = [[array mutableCopy] autorelease];
-	[dSelector setSourceArray:mutArray]; // !!!:nicholas:20040319
-	[dSelector setResultArray:mutArray]; // !!!:nicholas:20040319
-    [dSelector setSearchMode:SearchFilter];
-	[self updateViewLocations];
-	[self updateActionsNow];
-	[self showMainWindow:self];
-	[[self window] makeFirstResponder:dSelector];
+    // show the results list with the first pane empty
+    [self showArray:array withDirectObject:nil];
 }
 
 - (void)showArray:(NSArray *)array {
     // display the results list with these items
-    [self searchArray:array];
     // put the first item from the array into the first pane
-    [dSelector selectObjectValue:[array objectAtIndex:0]];
+    [self showArray:array withDirectObject:[array objectAtIndex:0]];
     [dSelector showResultView:self];
+}
+
+- (void)showArray:(NSArray *)array withDirectObject:(QSObject *)dObject {
+    [actionsUpdateTimer invalidate];
+    NSMutableArray *mutArray = [[array mutableCopy] autorelease];
+    [dSelector setSourceArray:mutArray];
+    [dSelector setResultArray:mutArray];
+    [dSelector setSearchMode:SearchFilter];
+    if (dObject) {
+        // show an item from this array if set
+        [dSelector selectObjectValue:[array objectAtIndex:0]];
+    } else {
+        // clear the first pane
+        [self clearObjectView:dSelector];
+    }
+    [self updateViewLocations];
+    [self updateActionsNow];
+    [self showMainWindow:self];
+    [[self window] makeFirstResponder:dSelector];
 }
 
 #pragma mark -
