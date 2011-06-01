16ae72b67292cd8102af2c5d7d4927975dc3a9c6
Rob McBroom <github@skurfer.com>
2011-06-01 15:08:44 -0400

    new HTML parser (based on Beautiful Soup)

--- Quicksilver/Resources/QSURLExtractor.py
+++ Quicksilver/Resources/QSURLExtractor.py
@@ -1,74 +1,50 @@
 #!/usr/bin/env python
-# encoding: utf-8
+# -*- encoding: utf-8 -*-
 """
 QSURLExtractor.py
 
-Created by Rob McBroom on 2010-04-13.
+Created by Rob McBroom on 2011-06-01.
+
+output tab separated lines with the following fields:
+    0 url
+    1 text
+    2 shortcut
+    3 imageurl
 """
 
-import sys
-import os
-from HTMLParser import HTMLParser, HTMLParseError
-
-class ExtractLinks(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.insideLinkTag = False
-    
-    def handle_starttag(self,tag,attrs):
-        # print 'start', tag, self.insideLinkTag
-        if tag == 'a':
-            if self.insideLinkTag:
-                # the previously started tag must not have been closed properly
-                # send it out and move on
-                self.printLink(self.thisLink)
-            self.thisLink = {
-                'url': str(),
-                'title': str(),
-                'image': str(),
-            }
-            self.insideLinkTag = True
-            for name, value in attrs:
-                if name == 'href':
-                    self.thisLink['url'] = value
-        if tag == 'img':
-            # look for URL and title of linked images
-            if self.insideLinkTag:
-                for name, value in attrs:
-                    if name == 'src':
-                        self.thisLink['image'] = value
-                    if name == 'title':
-                        self.thisLink['title'] = value
-                        break
-                    if name == 'alt':
-                        self.thisLink['title'] = value
-    
-    def handle_data(self, data):
-        # if there's anything other than whitespace
-        # and we're inside a link
-        if data.strip() and self.insideLinkTag:
-            self.thisLink['title'] = data
-    
-    def handle_endtag(self,tag):
-        # print 'end', tag, self.insideLinkTag
-        if tag == 'a' and self.insideLinkTag:
-            self.printLink(self.thisLink)
-            self.thisLink = {
-                'url': str(),
-                'title': str(),
-                'image': str(),
-            }
-            self.insideLinkTag = False
-    
-    def printLink(self, thisLink):
-        """print tab separated link attributes"""
-        print '{0}\t{1}\t\t{2}'.format(thisLink['url'], thisLink['title'], thisLink['image'])
+from os import path
+from BeautifulSoup import BeautifulSoup
+import fileinput
+from sys import stdout
+import codecs
+streamWriter = codecs.lookup('utf-8')[-1]
+stdout = streamWriter(stdout)
 
 if __name__ == '__main__':
-    import fileinput
     page = ''.join([line for line in fileinput.input()])
-    parser = ExtractLinks()
-    try:
-        parser.feed(page)
-    except HTMLParseError, e:
-        pass
+    soup = BeautifulSoup(page)
+    for link in soup.findAll('a', href=True):
+        ## initialize the link
+        thisLink = {
+            'url': link['href'],
+            'title': link.string,
+            'shortcut': '',
+            'image': '',
+        }
+        ## see if the link contains an image
+        img = link.find('img', src=True)
+        if img:
+            thisLink['image'] = img['src']
+            if thisLink['title'] == None:
+                # look for a title here if none exists
+                if img.has_key('title'):
+                    thisLink['title'] = img['title']
+                elif img.has_key('alt'):
+                    thisLink['title'] = img['alt']
+                else:
+                    thisLink['title'] = path.basename(img['src'])
+        ## if there's *still* no title (empty <a></a> tag), fall back to the URL
+        if thisLink['title'] == None:
+            thisLink['title'] = path.basename(link['href'])
+        ## print the result
+        print '%s\t%s\t%s\t%s' % (thisLink['url'], thisLink['title'], thisLink['shortcut'], thisLink['image'])
