31a6c819f389fbffa2ae41fab383f4ac176b8d31
Patrick Robertson <robertson.patrick@gmail.com>
2011-06-11 02:18:50 +1000

    created a new method that returns the full OS X version, updated all HTTP requests to use a new User Agent

--- Quicksilver/Code-QuickStepCore/QSDefines.h
+++ Quicksilver/Code-QuickStepCore/QSDefines.h
@@ -1,4 +1,6 @@
 
-#define kQSUserAgent @"Quicksilver (Blacktree, MacOSX) "
+#define kQSUserAgent [NSString stringWithFormat:@"Quicksilver %@ (Mac OS X %@)",\
+					 (NSString *)CFBundleGetValueForInfoDictionaryKey(CFBundleGetMainBundle(), kCFBundleVersionKey),\
+					 [NSApplication macOSXFullVersion]]
 
 #define QUERY_KEY @"***"
