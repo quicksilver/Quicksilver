1d630b3e324bce8a7ee3d46b7761ceab9f89ba0b
Etienne Samson <tiennou7@gmail.com>
2011-10-06 13:41:46 +0200

    Application update paths

--- Quicksilver/Code-QuickStepCore/QSPaths.h
+++ Quicksilver/Code-QuickStepCore/QSPaths.h
@@ -9,8 +9,8 @@
 #define psMainPlugInsLocation QSApplicationSupportSubPath(@"PlugIns/", NO)
 #define psMainPlugInsToInstallLocation QSApplicationSupportSubPath(@"PlugIns/Incoming/", NO)
 
-#define kCheckUpdateURL         @"http://qs0.qsapp.com/check.php"
-#define kDownloadUpdateURL      @"http://qs0.qsapp.com/download.php"
+#define kCheckUpdateURL         @"http://qs0.qsapp.com/plugins/check.php"
+#define kDownloadUpdateURL      @"http://qs0.qsapp.com/plugins/download.php"
 #define kPluginInfoURL          @"http://qs0.qsapp.com/plugins/info.php"
 #define kPluginDownloadURL      @"http://qs0.qsapp.com/plugins/download.php"
 
