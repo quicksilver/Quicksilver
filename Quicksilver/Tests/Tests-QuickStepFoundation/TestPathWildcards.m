4ef1d668792a8786cc2fc85783206e731a9ef8f5
Henning Jungkurth <henning.jungkurth@gmail.com>
2012-02-21 20:49:28 +0100

    Added unit testing framework.

---
+++ Quicksilver/Tests/Tests-QuickStepFoundation/TestPathWildcards.m
@@ -0,0 +1,18 @@
+//
+//  TestPathWildcards.m
+//  Quicksilver
+//
+//  Created by Henning Jungkurth on 21/2/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestPathWildcards.h"
+
+
+@implementation TestPathWildcards
+
+-(void) testNoWildcard {
+	
+}
+
+@end
