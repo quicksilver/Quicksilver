46bb4f8499755426644b8968f083c5543177d28d
Henning Jungkurth <henning.jungkurth@gmail.com>
2012-03-03 11:39:37 +0100

    Added the test architecture for the other frameworks (QSCore.framework, QSEffects.framework, QSInterface.framework).

---
+++ Quicksilver/Tests/Tests-QSInterface/InterfaceDummyTest.m
@@ -0,0 +1,14 @@
+//
+//  InterfaceDummyTest.m
+//  Quicksilver
+//
+//  Created by Henning Jungkurth on 3/3/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "InterfaceDummyTest.h"
+
+
+@implementation InterfaceDummyTest
+
+@end
