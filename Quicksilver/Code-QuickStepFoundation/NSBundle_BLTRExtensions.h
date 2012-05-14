b313518b763684bb90e57150ebb16a371768082f
Patrick Robertson <robertson.patrick@gmail.com>
2012-05-14 21:00:44 +0100

    Change all NSLocalizedString methods return the original string if no localisation is found

--- Quicksilver/Code-QuickStepFoundation/NSBundle_BLTRExtensions.h
+++ Quicksilver/Code-QuickStepFoundation/NSBundle_BLTRExtensions.h
@@ -20,11 +20,11 @@
 
 #undef NSLocalizedStringFromTable
 #define NSLocalizedStringFromTable(key, tbl, comment) \
-[[NSBundle mainBundle] safeLocalizedStringForKey:(key) value:nil table:(tbl)]
+[[NSBundle mainBundle] safeLocalizedStringForKey:(key) value:(key) table:(tbl)]
 
 #undef NSLocalizedStringFromTableInBundle
 #define NSLocalizedStringFromTableInBundle(key, tbl, bundle, comment) \
-[bundle safeLocalizedStringForKey:(key) value:nil table:(tbl)]
+[bundle safeLocalizedStringForKey:(key) value:(key) table:(tbl)]
 
 #undef NSLocalizedStringWithDefaultValue
 #define NSLocalizedStringWithDefaultValue(key, tbl, bundle, val, comment) \
