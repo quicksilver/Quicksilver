91ca8a755d21ed74b86cc7ae5a36f3658501f172
Patrick Robertson <robertson.patrick@gmail.com>
2012-06-02 11:55:05 +0100

    remove usesOpenGLAcceleration method and it's single use in QSAboutWindowController
    Based on work by neurolepsy

--- Quicksilver/Code-QuickStepFoundation/QSFoundation.h
+++ Quicksilver/Code-QuickStepFoundation/QSFoundation.h
@@ -41,7 +41,6 @@
 #import "NSTableView_BLTRExtensions.h"
 #import "NSString_BLTRExtensions.h"
 #import "NSSortDescriptor+BLTRExtensions.h"
-#import "NSScreen_BLTRExtensions.h"
 #import "NSPasteboard_BLTRExtensions.h"
 #import "NSObject+ReaperExtensions.h"
 #import "NSObject+BLTRExtensions.h"
