6f3914d794a6328fedb656ffd14e6736e698d5f1
Rob McBroom <github@skurfer.com>
2013-03-01 21:47:06 -0500

    add a few unit tests for QSObject

---
+++ Quicksilver/Tests/Tests-QSCore/TestQSObject.m
@@ -0,0 +1,116 @@
+//
+//  TestQSObject.m
+//  Quicksilver
+//
+//  Created by Rob McBroom on 2013/03/01.
+//
+//
+
+#import "TestQSObject.h"
+#import "QSTypes.h"
+#import "QSDefines.h"
+#import "QSObject.h"
+#import "QSObject_StringHandling.h"
+#import "QSObject_FileHandling.h"
+#import "QSObject_URLHandling.h"
+#import "QSObject_PropertyList.h"
+
+@implementation TestQSObject
+
+- (void)testStringObject
+{
+    NSString *exampleString = @"Example string in Quicksilver";
+    QSObject *object = [QSObject objectWithString:exampleString];
+    STAssertEqualObjects([object stringValue], exampleString, @"stringValue mismatch");
+    STAssertEqualObjects([object objectForType:QSTextType], exampleString, @"QSTextType mismatch");
+}
+
+- (void)testURLObject
+{
+    NSArray *exampleURLs = @[@"qsapp.com", @"http://www.qsapp.com/"];
+    for (NSString *url in exampleURLs) {
+        QSObject *object = [QSObject URLObjectWithURL:url title:nil];
+        STAssertTrue([object containsType:QSURLType] && [[object primaryType] isEqualToString:QSURLType], @"URL '%@' was not set up properly", url);
+    }
+    NSString *searchURL = [NSString stringWithFormat:@"http://www.qsapp.com/?q=%@&other_param=foo", QUERY_KEY];
+    QSObject *object = [QSObject URLObjectWithURL:searchURL title:@"Web Search"];
+    STAssertTrue([object containsType:QSSearchURLType] && [[object primaryType] isEqualToString:QSSearchURLType], @"URL '%@' was not recognized as a web search", searchURL);
+}
+
+- (void)testStringSniffing
+{
+    NSArray *shouldBeURL = @[@"localhost", @"localhost:1024", @"qsapp.com", @"http://qsapp.com/", @"http://hostname"];
+    for (NSString *url in shouldBeURL) {
+        QSObject *object = [QSObject objectWithString:url];
+        STAssertTrue([object containsType:QSURLType] && [[object primaryType] isEqualToString:QSURLType], @"'%@' was not recognized as a URL", url);
+    }
+    
+    NSArray *shouldNotBeURL = @[@"ordinary text", @"localhost:", @"http://localhost:", @"host.invalid.topleveldomain", @"http://host.invalid.topleveldomain"];
+    for (NSString *text in shouldNotBeURL) {
+        QSObject *object = [QSObject objectWithString:text];
+        STAssertTrue([[object primaryType] isEqualToString:QSTextType], @"'%@' was not recognized as plain text", text);
+    }
+    
+    NSString *mailto = @"mailto:example@fake.tld";
+    QSObject *email = [QSObject objectWithString:mailto];
+    STAssertTrue([[email primaryType] isEqualToString:QSEmailAddressType], @"'%@' was not recongnized as an e-mail address", mailto);
+    mailto = @"mailto:invalid address";
+    email = [QSObject objectWithString:mailto];
+    STAssertTrue([[email primaryType] isEqualToString:QSTextType], @"'%@' should not be treated as an e-mail address", mailto);
+    
+    NSString *calculation = @"=5*5";
+    QSObject *object = [QSObject objectWithString:calculation];
+    STAssertTrue([[object primaryType] isEqualToString:QSFormulaType], @"'%@' was not recognized as a caculation", calculation);
+}
+
+- (void)testObjectType
+{
+    NSDictionary *objectsAndTypes = @{
+        @"QSUnitTestStringType": @"string",
+        @"QSUnitTestDictionaryType": @{@"key": @"value"},
+        @"QSUnitTestArrayType": @[@"one", @"two", @"three"],
+        @"QSUnitTestExoticType": [NSImage imageNamed:NSImageNameUser]
+    };
+    QSObject *object = [QSObject makeObjectWithIdentifier:@"QSUnitTest:objectType"];
+    for (NSString *type in [objectsAndTypes allKeys]) {
+        id originalObject = [objectsAndTypes objectForKey:type];
+        [object setObject:originalObject forType:type];
+        id storedObject = [object objectForType:type];
+        STAssertEqualObjects(storedObject, originalObject, @"Stored object doesn't match original object. Class: '%@'", [originalObject class]);
+    }
+}
+
+- (void)testDisplayName
+{
+    NSString *name = @"Object Name";
+    NSString *label = @"Object Label";
+    QSObject *object = [QSObject makeObjectWithIdentifier:@"QSUnitTest:displayName"];
+    [object setName:name];
+    STAssertEqualObjects([object displayName], name, nil);
+    [object setLabel:label];
+    STAssertEqualObjects([object displayName], label, nil);
+    [object setName:label];
+    STAssertNil([object label], nil);
+    STAssertEqualObjects([object displayName], label, nil);
+}
+
+- (void)testCombinedObjects
+{
+    // combine some objects
+    // do some test on the combined object (count, arrayForType)
+    // split the object and see what comes out
+}
+
+- (void)testCacheExpiration
+{
+    // requires #1218
+//    NSString *cacheKey = @"temporaryData";
+//    NSString *data = @"string";
+//    QSObject *object = [QSObject makeObjectWithIdentifier:@"QSUnitTest:tempCache"];
+//    [object setObject:data forCache:cacheKey forTimeInterval:0.5];
+//    STAssertEqualObjects(data, [object objectForCache:cacheKey], nil);
+//    sleep(1);
+//    STAssertNil([object objectForCache:cacheKey], nil);
+}
+
+@end
