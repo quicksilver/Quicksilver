quicksilver/Quicksilver

commit 4919bcdb99ef0f7c01eeb7848f144f550fc64a01

always call `selectObjectValue:` when the object changes
`setObjectValue:` is lacking some smarts for comparing objects and
managing observers. `selectObjectValue:` takes care of that, as well as
the two lines that were removed.

Quicksilver/Code-QuickStepInterface/QSSearchObjectView.m
@@ -555,7 +555,7 @@ NSMutableDictionary *bindingsDict = nil;
 
 #pragma mark -
 #pragma mark Object Value
-- (void)selectObjectValue:(QSObject *)newObject {
+- (void)selectObjectValue:(id)newObject {
     QSObject *currentObject = [self objectValue];
     
     // resolve the current and new objects in order to compare them
@@ -580,8 +580,7 @@ NSMutableDictionary *bindingsDict = nil;
     [self clearSearch];
     [parentStack removeAllObjects];
     [self setResultArray:[NSMutableArray arrayWithObjects:newObject, nil]];
-    [super setObjectValue:newObject];
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"SearchObjectChanged" object:self];
+    [self selectObjectValue:newObject];
 }
 
 - (void)clearObjectValue {

Quicksilver/Code-QuickStepInterface/QSSearchObjectView.h
@@ -96,7 +96,7 @@ typedef enum QSSearchMode {
 
 - (void)clearObjectValue;
 - (void)moveSelectionBy:(NSInteger)d;
-- (void)selectObjectValue:( QSObject *)newObject ;
+- (void)selectObjectValue:(id)newObject ;
 - (void)pageScroll:(NSInteger)direction;
 
 - (NSMutableArray *)sourceArray;

commit 6c4cff7def77324d6b9bbbd76f6202c3eaa468de

fix a bug introduced by 4919bcdb99ef0f7c01eeb7848f144f550fc64a01 that meant the iObject wasn't always updated when activating triggers

Quicksilver/Code-QuickStepInterface/QSSearchObjectView.m
@@ -566,7 +566,7 @@ NSMutableDictionary *bindingsDict = nil;
         currentObject = [(QSRankedObject *)currentObject object];
     }
     // if the two objects are not the same, send an 'object chagned' notif
-	if (newObject != currentObject) {
+	if (newObject != currentObject || [newObject isKindOfClass:[QSAction class]]) {
         [[NSNotificationCenter defaultCenter] removeObserver:self name:QSObjectIconModified object:currentObject];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(objectIconModified:) name:QSObjectIconModified object:newObject];
 		[super setObjectValue:newObject];