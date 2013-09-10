quicksilver/Quicksilver

commit b8774cdd0c3c22bea4b4f3e00d8528cc9e09510b

Make that alloc-init call more straightforward.

Quicksilver/Code-App/QSController.m
@@ -275,8 +275,7 @@ static QSController *defaultController = nil;
 
 		QSWindow *quitWindow = nil;
 		if (!quitWindowController) {
-			quitWindowController = [NSWindowController alloc];
-			[quitWindowController initWithWindowNibName:@"QuitConfirm" owner:quitWindowController];
+			quitWindowController = [[NSWindowController alloc] initWithWindowNibName:@"QuitConfirm" owner:quitWindowController];
 
 			quitWindow = (QSBorderlessWindow *)[quitWindowController window];
 			[quitWindow setLevel:kCGPopUpMenuWindowLevel+1];

commit 148ef7c95951fa941714fc711769814b8d762129

Fix the never-quitable QS bug I introduced in b8774cdd0c3c22bea4b4f3e00d8528cc9e09510b.
Though a never-quitable QS sounds like a great idea ;-).

Quicksilver/Code-App/QSController.m
@@ -282,7 +282,7 @@ static QSController *defaultController = nil;
 
 		QSWindow *quitWindow = nil;
 		if (!quitWindowController) {
-			quitWindowController = [[NSWindowController alloc] initWithWindowNibName:@"QuitConfirm" owner:quitWindowController];
+			quitWindowController = [[NSWindowController alloc] initWithWindowNibName:@"QuitConfirm"];
 
 			quitWindow = (QSBorderlessWindow *)[quitWindowController window];
 			[quitWindow setLevel:kCGPopUpMenuWindowLevel+1];