de6a479db203ca9718a06ec74e04384367b8f42e
Etienne Samson <tiennou7@gmail.com>
2013-09-11 00:58:24 +0200

    `runOnMainQueueSync` => `QSGCDMainSync`, `runOnQueueSync` => `QSGCDQueueSync`
    + Bonus helpers.

--- Quicksilver/Code-QuickStepFoundation/QSGCD.h
+++ Quicksilver/Code-QuickStepFoundation/QSGCD.h
@@ -6,7 +6,54 @@
 //
 //
 
-extern const char* kQueueCatalogEntry;
+#ifndef __QSGCD__
+#define __QSGCD__
 
-void runOnMainQueueSync(void (^block)(void));
-void runOnQueueSync(dispatch_queue_t queue,void (^block)(void));
+extern const char *kQueueCatalogEntry;
+
+inline void QSGCDMainSync(void (^block)(void))
+{
+    if ([NSThread isMainThread]) {
+        block();
+    } else {
+        dispatch_sync(dispatch_get_main_queue(), block);
+    }
+}
+
+inline void QSGCDMainAsync(void (^block)(void))
+{
+    dispatch_async(dispatch_get_main_queue(), block);
+}
+
+inline void QSGCDQueueSync(dispatch_queue_t queue, void (^block)(void))
+{
+    if (dispatch_queue_get_label(queue) == dispatch_queue_get_label(dispatch_get_current_queue())) {
+        block();
+    } else {
+        dispatch_sync(queue, block);
+    }
+}
+
+inline void QSGCDQueueAsync(dispatch_queue_t queue, void (^block)(void))
+{
+    dispatch_async(queue, block);
+}
+
+inline void QSGCDSync(void (^block)(void))
+{
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    QSGCDQueueSync(queue, block);
+}
+
+inline void QSGCDAsync(void (^block)(void))
+{
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    QSGCDQueueAsync(queue, block);
+}
+
+// Remove those when the plugins are call-free
+// Don't forget to remove definitions in .m file
+void runOnMainQueueSync(void (^block)(void)) __attribute__((deprecated));
+void runOnQueueSync(dispatch_queue_t queue, void (^block)(void)) __attribute__((deprecated));
+
+#endif // __QSGCD__
