61b770739ce1a3e5979a2b0e15af86f8a229bb47
Etienne Samson <tiennou7@gmail.com>
2013-09-15 16:05:08 +0200

    Cruft cleanup.

--- Quicksilver/Code-QuickStepCore/QSCatalogEntry.h
+++ Quicksilver/Code-QuickStepCore/QSCatalogEntry.h
@@ -26,10 +26,10 @@
 - (BOOL)isSeparator;
 - (BOOL)isGroup;
 - (BOOL)isEditable;
-- (NSInteger) state;
-- (NSInteger) hasEnabledChildren;
 - (BOOL)isEnabled;
 - (void)setEnabled:(BOOL)enabled;
+- (NSInteger)state;
+- (NSInteger)hasEnabledChildren;
 - (void)setDeepEnabled:(BOOL)enabled;
 - (void)pruneInvalidChildren;
 - (NSArray *)leafIDs;
@@ -63,8 +63,7 @@
 
 - (NSDate *)indexDate;
 - (void)setIndexDate:(NSDate *)anIndexDate;
-//- (NSString *)countString;
 
-- (NSUInteger) count;
+- (NSUInteger)count;
 - (NSIndexPath *)catalogSetIndexPath;
 @end
