f28d6d8e1264770510f400851f4fc4b7f27141fe
Etienne Samson <tiennou7@gmail.com>
2013-11-28 20:55:00 +0100

    Transfer ownership correctly on that one.
    ```
    Leak: 0x10e2df330  size=48  zone: DefaultMallocZone_0x100319000   __NSCFString  ObjC  CoreFoundation  "/Volumes/Quicksilver"
    	Call stack: [thread 0x106122000]: | start_wqthread | _pthread_wqthread | _dispatch_worker_thread2 | _dispatch_client_callout | _dispatch_call_block_and_release | __42-[QSLibrarian scanCatalogIgnoringIndexes:]_block_invoke QSLibrarian.m:531 | -[QSCatalogEntry scanForced:] QSCatalogEntry.m:577 | -[QSCatalogEntry scanAndCache] QSCatalogEntry.m:526 | QSGCDQueueSync QSGCD.h:33 | _dispatch_barrier_sync_f_invoke | _dispatch_client_callout | __30-[QSCatalogEntry scanAndCache]_block_invoke QSCatalogEntry.m:532 | -[QSCatalogEntry scannedObjects] QSCatalogEntry.m:502 | -[QSDefaultsObjectSource objectsForEntry:] QSDefaultsObjectSource.m:85 | -[QSDefaultsObjectSource addObjectsForKeyList:keyNumber:ofType:inObject:toArray:] QSDefaultsObjectSource.m:102 | -[QSDefaultsObjectSource addObjectsForKeyList:keyNumber:ofType:inObject:toArray:] QSDefaultsObjectSource.m:95 | -[QSDefaultsObjectSource addObjectsForKeyList:keyNumber:ofType:inObject:toArray:] QSDefaultsObjectSource.m:102 | -[QSDefaultsObjectSource addObjectsForKeyList:keyNumber:ofType:inObject:toArray:] QSDefaultsObjectSource.m:118 | -[NDAlias(QSMods) quickPath] NDAlias+QSMods.m:15 | FSCopyAliasInfo | _FSCopyExtendedAliasInfoFromAliasPtr | CFStringCreateWithCString | __CFStringCreateImmutableFunnel3 | _CFRuntimeCreateInstance | malloc_zone_malloc
    ```

--- Quicksilver/Code-QuickStepFoundation/NDAlias+QSMods.m
+++ Quicksilver/Code-QuickStepFoundation/NDAlias+QSMods.m
@@ -12,6 +12,6 @@
 - (NSString *)quickPath {
 	CFStringRef pathString = nil;
 	FSCopyAliasInfo(aliasHandle, NULL, NULL, &pathString, NULL, NULL);
-	return (__bridge NSString *)pathString;
+	return (__bridge_transfer NSString *)pathString;
 }
 @end
