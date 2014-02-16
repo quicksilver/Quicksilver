e7060fdd9bd958d51c4e2dd17897bd066b5aaae2
Etienne Samson <tiennou7@gmail.com>
2014-02-16 17:03:54 +0100

    Add Delayed to the party.

--- Quicksilver/Code-QuickStepFoundation/QSGCD.h
+++ Quicksilver/Code-QuickStepFoundation/QSGCD.h
@@ -25,6 +25,12 @@ inline void QSGCDQueueAsync(dispatch_queue_t queue, void (^block)(void))
     dispatch_async(queue, block);
 }
 
+inline void QSGCDQueueDelayed(dispatch_queue_t queue, NSTimeInterval delay, void (^block)(void))
+{
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delay * NSEC_PER_SEC));
+    dispatch_after(popTime, queue, block);
+}
+
 inline void QSGCDMainSync(void (^block)(void))
 {
     QSGCDQueueSync(dispatch_get_main_queue(), block);
@@ -33,6 +39,11 @@ inline void QSGCDMainSync(void (^block)(void))
 inline void QSGCDMainAsync(void (^block)(void))
 {
     QSGCDQueueAsync(dispatch_get_main_queue(), block);
+}
+
+inline void QSGCDMainDelayed(NSTimeInterval delay, void(^block)(void))
+{
+    QSGCDQueueDelayed(dispatch_get_main_queue(), delay, block);
 }
 
 inline void QSGCDSync(void (^block)(void))
@@ -45,6 +56,12 @@ inline void QSGCDAsync(void (^block)(void))
 {
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     QSGCDQueueAsync(queue, block);
+}
+
+inline void QSGCDDelayed(NSTimeInterval delay, void (^block)(void))
+{
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    QSGCDQueueDelayed(queue, delay, block);
 }
 
 // Remove those when the plugins are call-free
