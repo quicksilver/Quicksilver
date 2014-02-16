a5780a6111ada0783eff393ff3dc6316f19c3274
Etienne Samson <tiennou7@gmail.com>
2014-02-16 17:01:34 +0100

    Reorder GCD methods based on specificity, and DRY.

--- Quicksilver/Code-QuickStepFoundation/QSGCD.h
+++ Quicksilver/Code-QuickStepFoundation/QSGCD.h
@@ -11,20 +11,6 @@
 
 extern const char *kQueueCatalogEntry;
 
-inline void QSGCDMainSync(void (^block)(void))
-{
-    if ([NSThread isMainThread]) {
-        block();
-    } else {
-        dispatch_sync(dispatch_get_main_queue(), block);
-    }
-}
-
-inline void QSGCDMainAsync(void (^block)(void))
-{
-    dispatch_async(dispatch_get_main_queue(), block);
-}
-
 inline void QSGCDQueueSync(dispatch_queue_t queue, void (^block)(void))
 {
     if (dispatch_queue_get_label(queue) == dispatch_queue_get_label(dispatch_get_current_queue())) {
@@ -37,6 +23,16 @@ inline void QSGCDQueueSync(dispatch_queue_t queue, void (^block)(void))
 inline void QSGCDQueueAsync(dispatch_queue_t queue, void (^block)(void))
 {
     dispatch_async(queue, block);
+}
+
+inline void QSGCDMainSync(void (^block)(void))
+{
+    QSGCDQueueSync(dispatch_get_main_queue(), block);
+}
+
+inline void QSGCDMainAsync(void (^block)(void))
+{
+    QSGCDQueueAsync(dispatch_get_main_queue(), block);
 }
 
 inline void QSGCDSync(void (^block)(void))
--- Quicksilver/Code-QuickStepFoundation/QSGCD.m
+++ Quicksilver/Code-QuickStepFoundation/QSGCD.m
@@ -20,9 +20,11 @@ void runOnQueueSync(dispatch_queue_t queue, void (^block)(void))
     QSGCDQueueSync(queue, block);
 }
 
-extern inline void QSGCDMainSync(void (^block)(void));
-extern inline void QSGCDMainAsync(void (^block)(void));
 extern inline void QSGCDQueueSync(dispatch_queue_t queue, void (^block)(void));
 extern inline void QSGCDQueueAsync(dispatch_queue_t queue, void (^block)(void));
+
+extern inline void QSGCDMainSync(void (^block)(void));
+extern inline void QSGCDMainAsync(void (^block)(void));
+
 extern inline void QSGCDSync(void (^block)(void));
 extern inline void QSGCDAsync(void (^block)(void));
