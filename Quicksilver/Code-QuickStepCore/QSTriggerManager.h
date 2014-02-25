5999d65804fc692b5d40cc6a6229dc2bcf007776
Rob McBroom <github@skurfer.com>
2014-02-25 15:24:01 -0500

    define the QSEventTriggerProvider protocol

--- Quicksilver/Code-QuickStepCore/QSTriggerManager.h
+++ Quicksilver/Code-QuickStepCore/QSTriggerManager.h
@@ -23,6 +23,48 @@
 - (void)trigger:(QSTrigger *)trigger setTriggerDescription:(NSString *)description;
 @end
 
+/**
+ *  QSEventTriggerProvider Protocol
+ *
+ *  Optional methods to define behavior when Event Triggers are enabled and disabled.
+ */
+@protocol QSEventTriggerProvider
+@optional;
+/**
+ *  If multiple events from this provider share common prerequisites, set them up here. This will be called before the first trigger using an event from this provider is enabled.
+ */
+- (void)enableEventProvider;
+/**
+ *  If multiple events from this provider share common prerequisites, tear them down here. This is called after the last trigger using an event from this provider is disabled.
+ */
+- (void)disableEventProvider;
+/**
+ *  Set up prerequisites for a specific event. This will be called *once* before the first trigger using the event is enabled.
+ *
+ *  @param event The identifier for an event, as defined under QSTriggerEvents in a plug-in's property list.
+ */
+- (void)enableEventObserving:(NSString *)event;
+/**
+ *  Tear down prerequisites for a specific event. This will be called *once* after the last trigger using the event is disabled.
+ *
+ *  @param event The identifier for an event, as defined under QSTriggerEvents in a plug-in's property list.
+ */
+- (void)disableEventObserving:(NSString *)event;
+/**
+ *  Set up observation for an event. This will be called for every trigger that uses the event when the trigger is enabled.
+ *
+ *  @param event   The identifier for an event, as defined under QSTriggerEvents in a plug-in's property list.
+ *  @param trigger The trigger being enabled.
+ */
+- (void)addObserverForEvent:(NSString *)event trigger:(QSTrigger *)trigger;
+/**
+ *  Tear down observation for an event. This will be called for every trigger that uses the event when the trigger is disabled.
+ *
+ *  @param event   The identifier for an event, as defined under QSTriggerEvents in a plug-in's property list.
+ *  @param trigger The trigger being disabled.
+ */
+- (void)removeObserverForEvent:(NSString *)event trigger:(QSTrigger *)trigger;
+@end
 
 //@interface NSObject (QSTriggerManager)
 //- (NSView *)settingsView;
