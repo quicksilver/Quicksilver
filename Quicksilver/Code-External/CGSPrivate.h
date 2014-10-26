5ae34fc3df32e87dd62bd564423d2f9d4c3a4d92
Etienne Samson <samson.etienne@gmail.com>
2014-10-26 12:34:57 +0100

    Make those NSIntegers so some warnings go away

--- Quicksilver/Code-External/CGSPrivate.h
+++ Quicksilver/Code-External/CGSPrivate.h
@@ -53,9 +53,9 @@
 */
 
 // Internal CoreGraphics typedefs
-typedef int		CGSConnection;
-typedef int		CGSWindow;
-typedef int		CGSValue;
+typedef NSInteger	CGSConnection;
+typedef NSInteger   CGSWindow;
+typedef NSInteger   CGSValue;
 
 //// CONSTANTS ////
 
