f66adb81251f984ea21eef2c016815983a03e38c
Rob McBroom <github@skurfer.com>
2014-12-13 14:08:38 -0500

    don't pass / to `imageNamed:`
    fixes #2002

--- Quicksilver/Code-QuickStepCore/QSResourceManager.h
+++ Quicksilver/Code-QuickStepCore/QSResourceManager.h
@@ -7,6 +7,7 @@ extern id QSRez;
 	NSString *resourceOverrideFolder;
 	NSDictionary *resourceOverrideList;
     dispatch_queue_t resourceQueue;
+    NSMutableSet *slashNames;
 }
 
 + (instancetype)sharedInstance;
