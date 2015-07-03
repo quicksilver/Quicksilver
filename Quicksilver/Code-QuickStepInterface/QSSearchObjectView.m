58fad24a5193f68fe838a02a822e736ac2770e8c
Rob McBroom <github@skurfer.com>
2015-07-02 23:11:50 -0400

    allow type-specific smart spacebar behavior

--- Quicksilver/Code-QuickStepInterface/QSSearchObjectView.m
+++ Quicksilver/Code-QuickStepInterface/QSSearchObjectView.m
@@ -1333,8 +1333,15 @@ NSMutableDictionary *bindingsDict = nil;
 {
 	NSInteger behavior = [[NSUserDefaults standardUserDefaults] integerForKey:@"QSSearchSpaceBarBehavior"];
 
-    QSObject * newSelectedObject = [[super objectValue] resolvedObject];
+    QSObject *newSelectedObject = [[super objectValue] resolvedObject];
     QSAction *action = [[self actionSelector] objectValue];
+    if (behavior == 7) {
+        // override smart defaults with type-specific behavior (if defined)
+        NSNumber *typeBehavior = [[[QSReg tableNamed:@"QSTypeDefinitions"] objectForKey:[newSelectedObject primaryType]] objectForKey:@"smartspace"];
+        if (typeBehavior) {
+            behavior = [typeBehavior integerValue];
+        }
+    }
 
 	switch(behavior) {
 		case 1: //Normal
@@ -1376,8 +1383,6 @@ NSMutableDictionary *bindingsDict = nil;
             }
             // Show child contents but only if object isn't a URL or text file
             else if ([newSelectedObject hasChildren] &&
-                    ![[newSelectedObject primaryType] isEqualToString:QSURLType] &&
-                    ![[newSelectedObject primaryType] isEqualToString:QSSearchURLType] &&
                     !QSTypeConformsTo([newSelectedObject fileUTI], (__bridge NSString *)kUTTypePlainText))
             {
                 [self moveRight:sender];
