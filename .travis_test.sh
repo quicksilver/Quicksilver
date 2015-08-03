90fa74835c1b0e41a4d00510e255ac095ec6d0ea
Patrick Robertson <robertson.patrick@gmail.com>
2015-08-03 12:10:28 -0400

    Don't run 'Quicksilver' UI tests in 'QS Distribution' schemes

--- .travis_test.sh
+++
@@ -1,3 +0,0 @@
-xctool -project Quicksilver/Quicksilver.xcodeproj -scheme "QuickStep Core" test
-xctool -project Quicksilver/Quicksilver.xcodeproj -scheme "QuickStep Foundation" test
-xctool -project Quicksilver/Quicksilver.xcodeproj -scheme "Core Support" test
