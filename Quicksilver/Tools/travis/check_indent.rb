5a546dca14b766342525f2af73ffffc03a71376e
Patrick Robertson <robertson.patrick@gmail.com>
2015-09-28 11:02:09 +0800

    Use tabs in check_indent... :S

--- Quicksilver/Tools/travis/check_indent.rb
+++ Quicksilver/Tools/travis/check_indent.rb
@@ -4,10 +4,11 @@
 require 'set'
 
 changed_lines = `git diff $TRAVIS_COMMIT_RANGE -- *.[mh]`.split("\n").select { |l| l.start_with? "+" }
+
 changed_lines.each do |line|
-    if /^\+ /.match(line) then
-      puts "Invalid line #{line}"
-      exit 1
-    end
+	if /^\+ /.match(line) then
+		puts "Invalid line #{line}"
+		exit 1
+	end
 end
 
