38a63f97abfbae30a981500e789faf3acd701c0c
Etienne Samson <samson.etienne@gmail.com>
2015-11-13 16:03:56 +0100

    Deprecate the duplicated frame-saving machinery and switch to NSWindow

--- Quicksilver/Code-QuickStepInterface/QSDockingWindow.m
+++ Quicksilver/Code-QuickStepInterface/QSDockingWindow.m
@@ -250,8 +250,8 @@
 }
 
 - (void)saveFrame {
-	if ([self autosaveName]) {
-		[self saveFrameUsingName:[self autosaveName]];
+	if ([self frameAutosaveName]) {
+		[self saveFrameUsingName:[self frameAutosaveName]];
 		[[NSUserDefaults standardUserDefaults] synchronize];
 	}
 }
@@ -266,11 +266,19 @@
 		[super orderOut:sender];
 	}
 }
-- (NSString *)autosaveName { return autosaveName;  }
+
+- (NSString *)autosaveName { return self.frameAutosaveName;  }
+
+- (BOOL)setFrameAutosaveName:(NSString *)name {
+    BOOL success = [super setFrameAutosaveName:name];
+    if (!success) return NO;
+
+    [self setFrameUsingName:self.frameAutosaveName force:YES];
+    [self updateTrackingRect:self];
+    return YES;
+}
 
 - (void)setAutosaveName:(NSString *)newAutosaveName {
-	autosaveName = newAutosaveName;
-	[self setFrameUsingName:autosaveName force:YES];
-	[self updateTrackingRect:self];
+    [self setFrameAutosaveName:newAutosaveName];
 }
 @end
