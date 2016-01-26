9e81cab99b3c7675266166b8d36220c7d492761b
Etienne Samson <samson.etienne@gmail.com>
2016-01-26 03:11:31 +0100

    Smarter script for local development

--- Quicksilver/Tools/travis/check_indent.rb
+++ Quicksilver/Tools/travis/check_indent.rb
@@ -3,14 +3,16 @@
 
 require 'set'
 
-changed_lines = `git diff $TRAVIS_COMMIT_RANGE -- *.[mh]`.split("\n").select { |l| l.start_with? "+" }
+commit_range = ENV['TRAVIS_COMMIT_RANGE'] || 'master'
+
+changed_lines = `git diff #{commit_range} -- *.[mh]`.split("\n").select { |l| l.start_with? "+" }
 
 comment_block = false
 
+invalid_lines = []
 changed_lines.each do |line|
 	if /^\+ /.match(line) and not comment_block then
-		puts "Invalid line #{line}"
-		exit 1
+		invalid_lines << line
 	elsif /^\+\s*?\/\*\*/.match(line) then
 		comment_block = true
 	elsif /^\+\s*?\*\*\//.match(line) then
@@ -18,3 +20,8 @@ changed_lines.each do |line|
 	end
 end
 
+exit 0 if invalid_lines.empty?
+puts "Invalid lines:"
+invalid_lines.each do |line|
+	puts line
+end
