f387c5d273ee6785bcd12f6ae0bdbde788881005
Patrick Robertson <robertson.patrick@gmail.com>
2016-01-30 08:52:07 +0800

    Fix recent documents order
    Add new 'SFLListItem class'

--- Quicksilver/Code-App/QSApp.m
+++ Quicksilver/Code-App/QSApp.m
@@ -45,8 +45,7 @@ BOOL QSApplicationCompletedLaunch = NO;
         //    A value transformer for checking if a given value is '1'. Used in the QSSearchPrefPane (Caps lock menu item)
         QSIntValueTransformer *intValueIsTwo = [[QSIntValueTransformer alloc] initWithInteger:2];
         [NSValueTransformer setValueTransformer:intValueIsTwo forName:@"IntegerValueIsTwo"];
-
-        
+		
         [[NSUserDefaults standardUserDefaults] registerDefaults:[NSDictionary dictionaryWithContentsOfFile:[[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"Contents/QSDefaults.plist"]]];
         done = YES;
     }
