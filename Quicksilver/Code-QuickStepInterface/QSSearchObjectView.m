c11df62b94393d0a453331ff337b4e4327e0ec43
Etienne Samson <samson.etienne@gmail.com>
2016-02-06 00:04:14 +0100

    Fix #2127

--- Quicksilver/Code-QuickStepInterface/QSSearchObjectView.m
+++ Quicksilver/Code-QuickStepInterface/QSSearchObjectView.m
@@ -748,8 +748,8 @@ NSMutableDictionary *bindingsDict = nil;
 		editorFrame.origin = NSMakePoint(NSHeight(titleFrame) /16, NSHeight(titleFrame)/16);
         editorFrame = NSIntegralRect(editorFrame);
 
-        [[self textModeEditor] setMaxSize:NSMakeSize(FLT_MAX, FLT_MAX)];
-        [[self textModeEditor] setFocusRingType:NSFocusRingTypeNone];        
+		[[self textModeEditor] setMaxSize:titleFrame.size];
+		[[self textModeEditor] setFocusRingType:NSFocusRingTypeNone];
         [[self textModeEditor] setDelegate: self];
         [[self textModeEditor] setAllowsUndo:YES];
         [[self textModeEditor] setHorizontallyResizable: YES];
