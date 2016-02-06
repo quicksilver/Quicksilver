dfde39f0788eb3b3e5577ff410ecb460d47bd680
Etienne Samson <samson.etienne@gmail.com>
2016-02-06 03:01:54 +0100

    Beef up check_indent

--- Quicksilver/Tools/travis/check_indent.rb
+++ Quicksilver/Tools/travis/check_indent.rb
@@ -3,25 +3,44 @@
 
 require 'set'
 
-commit_range = ENV['TRAVIS_COMMIT_RANGE'] || 'master'
+commit_range = ENV['TRAVIS_COMMIT_RANGE'] || 'master..HEAD'
 
-changed_lines = `git diff #{commit_range} -- *.[mh]`.split("\n").select { |l| l.start_with? "+" }
+puts "check_indent.rb: commit range #{commit_range}"
 
+invalid_files = {}
 comment_block = false
-
-invalid_lines = []
+last_file = nil
+last_lines = nil
+changed_lines = `git diff #{commit_range} -- *.[mh]`.lines
 changed_lines.each do |line|
-	if /^\+ /.match(line) and not comment_block then
-		invalid_lines << line
-	elsif /^\+\s*?\/\*\*/.match(line) then
+	case line
+	when /^\+\+\+ [^\/]+\/(.*)$/
+		last_file = $1
+	when /^@@ ([-+]\d*,\d*) ([-+]\d*,\d*) @@/
+		last_lines = [$1, $2]
+	when /^\+ /
+		invalid_files[last_file] ||= {}
+		invalid_files[last_file][last_lines] ||= []
+		invalid_files[last_file][last_lines] << line unless comment_block
+	when /^\+\s*?\/\*\*/
 		comment_block = true
-	elsif /^\+\s*?\*\*\//.match(line) then
+	when /^\+\s*?\*\*\// then
 		comment_block = false
 	end
 end
 
-exit 0 if invalid_lines.empty?
-puts "Invalid lines:"
-invalid_lines.each do |line|
-	puts line
+if invalid_files.empty?
+	puts "All indent correct!"
+	exit 0
+end
+
+puts "The following files have incorrect indent:"
+invalid_files.each do |file, lines|
+	puts "⚠ in file #{file}:"
+	lines.each do |line_no, lines|
+		puts "⚠ around #{line_no[0]}..#{line_no[1]}:"
+		lines.each do |line|
+			puts line
+		end
+	end
 end
