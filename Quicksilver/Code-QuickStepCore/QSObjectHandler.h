c2d90a57ea7d1a34118da2a3f5fcadb63e31ad5b
Etienne Samson <samson.etienne@gmail.com>
2016-07-22 19:16:28 +0000

    Also make QSObjectHandler a formal protocol

---
+++ Quicksilver/Code-QuickStepCore/QSObjectHandler.h
@@ -0,0 +1,36 @@
+//
+//  QSObjectHandler.h
+//  Quicksilver
+//
+//  Created by Etienne on 22/07/2016.
+//
+//
+
+@class QSObject, QSBasicObject;
+
+@protocol QSObjectHandler <NSObject>
+- (BOOL)objectHasChildren:(QSObject *)object;
+- (BOOL)objectHasValidChildren:(QSObject *)object;
+
+- (BOOL)loadChildrenForObject:(QSObject *)object;
+- (NSArray *)childrenForObject:(QSObject *)object;
+- (QSObject *)parentOfObject:(QSObject *)object;
+- (NSString *)detailsOfObject:(QSObject *)object;
+- (NSString *)identifierForObject:(QSObject *)object;
+- (NSString *)kindOfObject:(QSObject *)object;
+- (void)setQuickIconForObject:(QSObject *)object;
+- (BOOL)loadIconForObject:(QSObject *)object;
+- (BOOL)drawIconForObject:(QSObject *)object inRect:(NSRect)rect flipped:(BOOL)flipped;
+
+- (id)dataForObject:(QSObject *)object pasteboardType:(NSString *)type;
+- (NSDragOperation)operationForDrag:(id <NSDraggingInfo>)sender ontoObject:(QSObject *)dObject withObject:(QSBasicObject *)iObject;
+- (NSString *)actionForDragMask:(NSDragOperation)operation ontoObject:(QSObject *)dObject withObject:(QSBasicObject *)iObject;
+
+- (NSArray *)actionsForDirectObject:(QSObject *)dObject indirectObject:(QSObject *)iObject;
+
+- (NSAppleEventDescriptor *)AEDescriptorForObject:(QSObject *)object;
+
+- (QSObject *)initFileObject:(QSObject *)object ofType:(NSString *)type QS_DEPRECATED NS_RETURNS_NOT_RETAINED;
+@end
+
+
