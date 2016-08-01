581f05d7ff20479690e31207844a349da8697708
Patrick Robertson <robertson.patrick@gmail.com>
2016-08-01 08:37:16 +0800

    Add test for iSelector closing

--- Quicksilver/Quicksilver Tests/Quicksilver_Tests.m
+++ Quicksilver/Quicksilver Tests/Quicksilver_Tests.m
@@ -19,6 +19,10 @@
 @implementation Quicksilver_Tests
 
 /**
+ * In order to run these tests, you must select 'Quicksilver' as the scheme, as opposed to 'Quicksilver Distribution'
+ */
+
+/**
  *  This is a test for bug #670, #203
  */
 - (void)testClearingFirstPane
@@ -70,6 +74,61 @@
     [[i dSelector] keyDown:typeAEvent];
     
     XCTAssertEqualObjects(@"a", [[i dSelector] searchString], @"The previous search string was not cleared when invoking a 'search children' command");
+}
+
+- (bool)isViewVisible:(QSSearchObjectView *)v forController:(QSInterfaceController *)i {
+	return [[[[i window] contentView] subviews] containsObject:v];
+}
+/**
+ * This is a test for #1468 and one that cropped up in #2249
+ * This test is equivalent to you typing "a" ⇥ "open with", checking the iSelector is visible,
+ * then typing ⌃U and checking the iSelector is closed
+ */
+- (void)testThirdPaneClosingBehaviour {
+	QSInterfaceController *i = [(QSController *)[NSApp delegate] interfaceController];
+	[i activate:nil];
+	NSEvent *typeAEvent = [NSEvent keyEventWithType:10 location:NSMakePoint(0, 0) modifierFlags:256 timestamp:15127.081604936 windowNumber:[[i window] windowNumber] context:nil characters:@"a" charactersIgnoringModifiers:@"a" isARepeat:NO keyCode:0];
+	[[i dSelector] keyDown:typeAEvent];
+	
+	// dSelector is populated with an object
+	XCTAssertNotNil([[i dSelector] objectValue]);
+	
+	// aSelector is nil until the actions timer has fired
+	XCTAssertNil([[i aSelector] objectValue]);
+
+	// UI tests hack: force the actions timer to fire now
+	[i fireActionUpdateTimer];
+	XCTAssertNotNil([[i aSelector] objectValue]);
+	
+	// the iSelector should be closed
+	XCTAssertFalse([self isViewVisible:[i iSelector] forController:i]);
+	
+	NSEvent *searchForActionEvent = [NSEvent keyEventWithType:10 location:NSMakePoint(0, 0) modifierFlags:256 timestamp:15127.081604936 windowNumber:[[i window] windowNumber] context:nil characters:@"open with" charactersIgnoringModifiers:@"open with" isARepeat:NO keyCode:0];
+	[[i aSelector] keyDown:searchForActionEvent];
+	XCTAssertFalse([[i iSelector] isHidden]);
+	// iSelector should now be visible
+	XCTAssertTrue([self isViewVisible:[i iSelector] forController:i]);
+	
+	// Clear the first pane (use ⌃U is easiest)
+	NSEvent *clearEvent = [NSEvent keyEventWithType:10 location:NSMakePoint(0, 0) modifierFlags:NSControlKeyMask timestamp:15127.081604936 windowNumber:[[i window] windowNumber] context:nil characters:@"u" charactersIgnoringModifiers:@"u" isARepeat:NO keyCode:32];
+	[[i dSelector] keyDown:clearEvent];
+
+
+	XCTAssertNil([[i dSelector] objectValue]);
+	
+	// aSelector still has object until the action timer is fired
+	XCTAssertNotNil([[i aSelector] objectValue]);
+	// iSeletor still visible
+	XCTAssertTrue([self isViewVisible:[i iSelector] forController:i]);
+
+	// UI tests hack: force the actions timer to fire now
+	[i fireActionUpdateTimer];
+	
+	XCTAssertNil([[i aSelector] objectValue]);
+	// the iSelector should be closed
+	XCTAssertFalse([self isViewVisible:[i iSelector] forController:i]);
+	
+	
 }
 
 @end
