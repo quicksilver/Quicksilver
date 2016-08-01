6d38a23ae622d1a5756b1cdf618327c0e69ac63d
Etienne Samson <samson.etienne@gmail.com>
2016-08-01 21:22:03 +0200

    Deprecate some delegate methods that have Cocoa replacements

--- Quicksilver/Code-QuickStepInterface/QSTableView.h
+++ Quicksilver/Code-QuickStepInterface/QSTableView.h
@@ -9,14 +9,14 @@
 - (BOOL)tableView:(QSTableView *)aTableView shouldDrawRow:(NSInteger)rowIndex inClipRect:(NSRect)clipRect;
 - (BOOL)tableView:(QSTableView *)aTableView rowIsSeparator:(NSInteger)rowIndex;
 - (NSMenu *)tableView:(QSTableView*)tableView menuForTableColumn:(NSTableColumn *)column row:(NSInteger)row;
-- (void)tableView:(QSTableView *)tv dropEndedWithOperation:(NSDragOperation)operation;
+- (void)tableView:(QSTableView *)tv dropEndedWithOperation:(NSDragOperation)operation QS_DEPRECATED_MSG("It has neved been called on the delegate");
 - (void)drawSeparatorForRow:(NSInteger)rowIndex clipRect:(NSRect)clipRect;
 @end
 
 @protocol QSTableViewDataSource <NSTableViewDataSource>
 
 @optional
-- (void)tableView:(QSTableView *)aTableView dropEndedWithOperation:(NSDragOperation)operation;
+- (void)tableView:(QSTableView *)aTableView dropEndedWithOperation:(NSDragOperation)operation QS_DEPRECATED_MSG("Use -tableView:draggingSession:... and friends");
 @end
 
 @interface QSTableView : NSTableView {
@@ -28,8 +28,8 @@
 }
 - (NSColor *)highlightColor;
 - (void)setHighlightColor:(NSColor *)aHighlightColor;
-- (id)draggingDelegate;
-- (void)setDraggingDelegate:(id)aDraggingDelegate;
+- (id)draggingDelegate QS_DEPRECATED_MSG("Use -tableView:draggingSession:... and friends");
+- (void)setDraggingDelegate:(id)aDraggingDelegate QS_DEPRECATED_MSG("Use -tableView:draggingSession:... and friends");;
 - (void)setOpaque:(BOOL)flag;
 - (id <QSTableViewDelegate>)delegate;
 - (id <QSTableViewDataSource>)dataSource;
