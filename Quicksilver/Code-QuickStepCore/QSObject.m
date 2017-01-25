f6fc2bf2529ee438b0304cfe3e90aaf52a2b1c5c
Rob McBroom <github@skurfer.com>
2017-01-24 23:11:07 -0500

    speed up isEqual
    access identifier iVars for the objects directly

--- Quicksilver/Code-QuickStepCore/QSObject.m
+++ Quicksilver/Code-QuickStepCore/QSObject.m
@@ -92,19 +92,20 @@ NSSize QSMaxIconSize;
     anObject = [anObject object];
   }
 	if (self == anObject) return YES;
-	if (([self identifier] || [anObject identifier]) && ![[self identifier] isEqualToString:[anObject identifier]]) return NO;
+	QSObject *otherObject = (QSObject *)anObject;
+	if ((identifier || otherObject->identifier) && ![identifier isEqualToString:otherObject->identifier]) return NO;
 	if ([self count] > 1) {
-		if ([self count] != [(QSObject *)anObject count]) {
+		if ([self count] != [otherObject count]) {
 			return NO;
 		}
 		NSSet *myObjects = [NSSet setWithArray:[self splitObjects]];
-		NSSet *otherObjects = [NSSet setWithArray:[anObject splitObjects]];
+		NSSet *otherObjects = [NSSet setWithArray:[otherObject splitObjects]];
 		if (![myObjects isEqualToSet:otherObjects]) {
 			return NO;
 		}
 	} else {
 		for(NSString *key in data) {
-			if (![[data objectForKey:key] isEqual:[anObject objectForType:key]]) return NO;
+			if (![[data objectForKey:key] isEqual:[otherObject objectForType:key]]) return NO;
 		}
 	}
 	return YES;
