0ee2944b3c789fc8f51a97b607a2ba89f296d404
Etienne Samson <samson.etienne@gmail.com>
2017-04-16 00:30:30 +0200

    Replace the Foundation version of our algo with the CF one

--- Quicksilver/Code-QuickStepFoundation/NSString_BLTRExtensions.m
+++ Quicksilver/Code-QuickStepFoundation/NSString_BLTRExtensions.m
@@ -43,49 +43,10 @@ NSComparisonResult prefixCompare(NSString *aString, NSString *bString) {
 }
 
 - (CGFloat) scoreForAbbreviation:(NSString *)abbreviation inRange:(NSRange)searchRange fromRange:(NSRange)abbreviationRange hitMask:(NSMutableIndexSet *)mask {
-	CGFloat score, remainingScore;
-	NSInteger i, j;
-	NSRange matchedRange, remainingSearchRange;
-	if (!abbreviationRange.length) return 0.9; //deduct some points for all remaining letters
-	if (abbreviationRange.length>searchRange.length) return 0.0;
-	for (i = abbreviationRange.length; i>0; i--) { //Search for steadily smaller portions of the abbreviation
-		matchedRange = [self rangeOfString:[abbreviation substringWithRange:NSMakeRange(abbreviationRange.location, i)] options:NSCaseInsensitiveSearch range:searchRange];
+	CFRange strRange = CFRangeMake(searchRange.location, searchRange.length);
+	CFRange abbrRange = CFRangeMake(abbreviationRange.location, abbreviationRange.length);
 
-		if (matchedRange.location == NSNotFound || matchedRange.location+abbreviationRange.length>NSMaxRange(searchRange)) continue;
-
-		if (mask) [mask addIndexesInRange:matchedRange];
-
-		remainingSearchRange.location = NSMaxRange(matchedRange);
-		remainingSearchRange.length = NSMaxRange(searchRange) -remainingSearchRange.location;
-
-		// Search what is left of the string with the rest of the abbreviation
-		remainingScore = [self scoreForAbbreviation:abbreviation inRange:remainingSearchRange fromRange:NSMakeRange(abbreviationRange.location+i, abbreviationRange.length-i) hitMask:mask];
-		if (remainingScore) {
-			score = remainingSearchRange.location-searchRange.location;
-			// ignore skipped characters if is first letter of a word
-			if (matchedRange.location>searchRange.location) {//if some letters were skipped
-				if ([[NSCharacterSet whitespaceCharacterSet] characterIsMember:[self characterAtIndex:matchedRange.location-1]]) {
-					for (j = matchedRange.location-2; j >= (NSInteger) searchRange.location; j--) {
-						if ([[NSCharacterSet whitespaceCharacterSet] characterIsMember:[self characterAtIndex:j]]) score--;
-						else score -= 0.15;
-					}
-				} else if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:matchedRange.location]]) {
-					for (j = matchedRange.location-1; j >= (NSInteger) searchRange.location; j--) {
-						if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:j]])
-							score--;
-						else
-							score -= 0.15;
-					}
-				} else {
-					score -= (matchedRange.location-searchRange.location)/2;
-				}
-			}
-			score += remainingScore*remainingSearchRange.length;
-			score /= searchRange.length;
-			return score;
-		}
-	}
-	return 0;
+	return QSScoreForAbbreviationWithRanges((__bridge CFStringRef)self, (__bridge CFStringRef)abbreviation, mask, strRange, abbrRange);
 }
 
 /* FIXME: This can be removed sometimes */
