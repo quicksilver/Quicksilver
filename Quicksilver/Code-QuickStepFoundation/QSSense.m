507f212b7150a09e98015869017dfcb735c231c0
Etienne Samson <samson.etienne@gmail.com>
2017-04-16 00:23:34 +0200

    Reindent and cleanups

--- Quicksilver/Code-QuickStepFoundation/QSSense.m
+++ Quicksilver/Code-QuickStepFoundation/QSSense.m
@@ -17,74 +17,80 @@
 CGFloat QSScoreForAbbreviationWithRanges(CFStringRef str, CFStringRef abbr, id mask, CFRange strRange, CFRange abbrRange);
 
 CGFloat QSScoreForAbbreviation(CFStringRef str, CFStringRef abbr, id mask) {
-	return QSScoreForAbbreviationWithRanges(str, abbr, mask, CFRangeMake(0, CFStringGetLength(str) ), CFRangeMake(0, CFStringGetLength(abbr)));
+	return QSScoreForAbbreviationWithRanges(str, abbr, mask, CFRangeMake(0, CFStringGetLength(str)), CFRangeMake(0, CFStringGetLength(abbr)));
 }
 
 CGFloat QSScoreForAbbreviationWithRanges(CFStringRef str, CFStringRef abbr, id mask, CFRange strRange, CFRange abbrRange) {
-    
 	if (!abbrRange.length)
-        return IGNORED_SCORE; //deduct some points for all remaining letters
-    
+		return IGNORED_SCORE; //deduct some points for all remaining letters
+
 	if (abbrRange.length > strRange.length)
-        return 0.0;
-	
+		return 0.0;
+
+	static CFCharacterSetRef wordSeparator = NULL;
+	static CFCharacterSetRef uppercase = NULL;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		wordSeparator = CFCharacterSetCreateMutableCopy(NULL, CFCharacterSetGetPredefined(kCFCharacterSetWhitespace));
+		CFCharacterSetAddCharactersInString((CFMutableCharacterSetRef)wordSeparator, (CFStringRef)@".");
+
+		uppercase = CFCharacterSetGetPredefined(kCFCharacterSetUppercaseLetter);
+	});
+
 	// Create an inline buffer version of str.  Will be used in loop below
 	// for faster lookups.
 	CFStringInlineBuffer inlineBuffer;
 	CFStringInitInlineBuffer(str, &inlineBuffer, strRange);
 	CFLocaleRef userLoc = CFLocaleCopyCurrent();
 
-    CGFloat score = 0.0, remainingScore = 0.0;
-	NSInteger i, j;
+	CGFloat score = 0.0, remainingScore = 0.0;
+	CFIndex i, j;
 	CFRange matchedRange, remainingStrRange, adjustedStrRange = strRange;
-    
-	for (i = abbrRange.length; i > 0; i--) { //Search for steadily smaller portions of the abbreviation
+
+	// Search for steadily smaller portions of the abbreviation
+	for (i = abbrRange.length; i > 0; i--) {
 		CFStringRef curAbbr = CFStringCreateWithSubstring (NULL, abbr, CFRangeMake(abbrRange.location, i) );
-		//terminality
-		//axeen
+		// terminality
+		// axeen
+
 		BOOL found = CFStringFindWithOptionsAndLocale(str, curAbbr,
-                                                      CFRangeMake(adjustedStrRange.location, adjustedStrRange.length - abbrRange.length + i),
-                                                      kCFCompareCaseInsensitive | kCFCompareDiacriticInsensitive | kCFCompareLocalized,
-                                                      userLoc, &matchedRange);
+													  CFRangeMake(adjustedStrRange.location, adjustedStrRange.length - abbrRange.length + i),
+													  kCFCompareCaseInsensitive | kCFCompareDiacriticInsensitive | kCFCompareLocalized,
+													  userLoc, &matchedRange);
 		CFRelease(curAbbr);
-		
+
 		if (!found) {
 			continue;
 		}
-		if (matchedRange.location + abbrRange.length > strRange.location + strRange.length) {
-			continue;
-		}
-		
+
 		if (mask) {
 			[mask addIndexesInRange:NSMakeRange(matchedRange.location, matchedRange.length)];
 		}
-		
+
 		remainingStrRange.location = matchedRange.location + matchedRange.length;
 		remainingStrRange.length = strRange.location + strRange.length - remainingStrRange.location;
-		
+
 		// Search what is left of the string with the rest of the abbreviation
-		remainingScore = QSScoreForAbbreviationWithRanges(str, abbr, mask, remainingStrRange, CFRangeMake(abbrRange.location + i, abbrRange.length - i) );
-		
+		remainingScore = QSScoreForAbbreviationWithRanges(str, abbr, mask, remainingStrRange, CFRangeMake(abbrRange.location + i, abbrRange.length - i));
+
 		if (remainingScore) {
 			score = remainingStrRange.location-strRange.location;
 			// ignore skipped characters if is first letter of a word
-			if (matchedRange.location>strRange.location) {//if some letters were skipped
-				static CFCharacterSetRef wordSeparator = NULL;
-				if (!wordSeparator)
-				{
-				  wordSeparator = CFCharacterSetCreateMutableCopy(NULL, CFCharacterSetGetPredefined(kCFCharacterSetWhitespace));
-				  CFCharacterSetAddCharactersInString((CFMutableCharacterSetRef)wordSeparator, (CFStringRef)@".");
-				}
-				static CFCharacterSetRef uppercase = NULL;
-				if (!uppercase) uppercase = CFCharacterSetGetPredefined(kCFCharacterSetUppercaseLetter);
-				if (CFCharacterSetIsCharacterMember(wordSeparator, CFStringGetCharacterFromInlineBuffer(&inlineBuffer, matchedRange.location-1) )) {
-					for (j = matchedRange.location-2; j >= (NSInteger) strRange.location; j--) {
-						if (CFCharacterSetIsCharacterMember(wordSeparator, CFStringGetCharacterFromInlineBuffer(&inlineBuffer, j) )) score--;
-						else score -= SKIPPED_SCORE;
+			if (matchedRange.location > strRange.location) {
+				// if some letters were skipped
+				UniChar previousChar = CFStringGetCharacterFromInlineBuffer(&inlineBuffer, matchedRange.location - 1);
+				UniChar character = CFStringGetCharacterFromInlineBuffer(&inlineBuffer, matchedRange.location);
+				if (CFCharacterSetIsCharacterMember(wordSeparator, previousChar)) {
+					// We're on the first letter of a word
+					for (j = matchedRange.location - 2; j >= strRange.location; j--) {
+						if (CFCharacterSetIsCharacterMember(wordSeparator, CFStringGetCharacterFromInlineBuffer(&inlineBuffer, j)))
+							score--;
+						else
+							score -= SKIPPED_SCORE;
 					}
-				} else if (CFCharacterSetIsCharacterMember(uppercase, CFStringGetCharacterFromInlineBuffer(&inlineBuffer, matchedRange.location) )) {
-					for (j = matchedRange.location-1; j >= (NSInteger) strRange.location; j--) {
-						if (CFCharacterSetIsCharacterMember(uppercase, CFStringGetCharacterFromInlineBuffer(&inlineBuffer, j) ))
+				} else if (CFCharacterSetIsCharacterMember(uppercase, character)) {
+					for (j = matchedRange.location - 1; j >= strRange.location; j--) {
+						if (CFCharacterSetIsCharacterMember(uppercase, CFStringGetCharacterFromInlineBuffer(&inlineBuffer, j)))
 							score--;
 						else
 							score -= SKIPPED_SCORE;
@@ -93,12 +99,12 @@ CGFloat QSScoreForAbbreviationWithRanges(CFStringRef str, CFStringRef abbr, id m
 					score -= (matchedRange.location-strRange.location)/2;
 				}
 			}
-			score += remainingScore*remainingStrRange.length;
+			score += remainingScore * remainingStrRange.length;
 			score /= strRange.length;
-            CFRelease(userLoc);
+			CFRelease(userLoc);
 			return score;
 		}
 	}
-    CFRelease(userLoc);
+	CFRelease(userLoc);
 	return 0;
 }
