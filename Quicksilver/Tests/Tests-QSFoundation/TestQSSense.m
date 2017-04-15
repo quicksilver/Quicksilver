175b05f00f042b87a83bde35400faea140a13922
Etienne Samson <samson.etienne@gmail.com>
2017-04-16 01:37:17 +0200

    Remove duplicate tests

--- Quicksilver/Tests/Tests-QSFoundation/TestQSSense.m
+++ Quicksilver/Tests/Tests-QSFoundation/TestQSSense.m
@@ -42,32 +42,6 @@ const float ACC = 0.00001;
 	XCTAssertEqualWithAccuracy(score, 0.63636, ACC);
 }
 
-- (void)testScoreSimpleNS {
-	NSString *str = @"Test string";
-	CGFloat score = 0;
-
-	score =	[str scoreForAbbreviation:@"t"];
-	XCTAssertEqualWithAccuracy(score, 0.90909, ACC);
-
-	score = [str scoreForAbbreviation:@"ts"];
-	XCTAssertEqualWithAccuracy(score, 0.92727, ACC);
-
-	score = [str scoreForAbbreviation:@"te"];
-	XCTAssertEqualWithAccuracy(score, 0.91818, ACC);
-
-	score = [str scoreForAbbreviation:@"tet"];
-	XCTAssertEqualWithAccuracy(score, 0.93636, ACC);
-
-	score = [str scoreForAbbreviation:@"str"];
-	XCTAssertEqualWithAccuracy(score, 0.91818, ACC);
-
-	score = [str scoreForAbbreviation:@"tstr"];
-	XCTAssertEqualWithAccuracy(score, 0.93181, ACC);
-
-	score = [str scoreForAbbreviation:@"ng"];
-	XCTAssertEqualWithAccuracy(score, 0.63636, ACC);
-}
-
 - (void)testScoreLongString {
 	CFStringRef str = CFSTR("This is a really long test string for testing");
 	CGFloat score = 0;
@@ -97,38 +71,6 @@ const float ACC = 0.00001;
 	XCTAssertEqualWithAccuracy(score, 0.75888, ACC);
 
 	score = QSScoreForAbbreviation(str, CFSTR("ng"), nil);
-	XCTAssertEqualWithAccuracy(score, 0.74666, ACC);
-}
-
-- (void)testScoreLongStringNS {
-	NSString *str = @"This is a really long test string for testing";
-	CGFloat score = 0;
-
-	score = [str scoreForAbbreviation:@"t"];
-	XCTAssertEqualWithAccuracy(score, 0.90222, ACC);
-
-	score = [str scoreForAbbreviation:@"ts"];
-	XCTAssertEqualWithAccuracy(score, 0.88666, ACC);
-
-	score = [str scoreForAbbreviation:@"te"];
-	XCTAssertEqualWithAccuracy(score, 0.80777, ACC);
-
-	score = [str scoreForAbbreviation:@"tet"];
-	XCTAssertEqualWithAccuracy(score, 0.81222, ACC);
-
-	score = [str scoreForAbbreviation:@"str"];
-	XCTAssertEqualWithAccuracy(score, 0.78555, ACC);
-
-	score = [str scoreForAbbreviation:@"tstr"];
-	XCTAssertEqualWithAccuracy(score, 0.78888, ACC);
-
-	score = [str scoreForAbbreviation:@"testi"];
-	XCTAssertEqualWithAccuracy(score, 0.74000, ACC);
-
-	score = [str scoreForAbbreviation:@"for"];
-	XCTAssertEqualWithAccuracy(score, 0.75888, ACC);
-
-	score = [str scoreForAbbreviation:@"ng"];
 	XCTAssertEqualWithAccuracy(score, 0.74666, ACC);
 }
 
@@ -184,64 +126,7 @@ const float ACC = 0.00001;
 	XCTAssertEqualObjects(indexes, results);
 }
 
-- (void)testLongStringNS {
-	NSString *str = @"This excellent string tells us an interesting story";
-	NSRange strRange = NSMakeRange(0, 27); // tells^
-	NSString *abbr = @"test";
-	NSRange abbrRange = NSMakeRange(0, [abbr length]);
-	CGFloat score = 0;
-	const int STEP = 4;
-	NSMutableIndexSet *indexes = [[NSMutableIndexSet alloc] init];
-	NSIndexSet *results = nil;
-
-	score = [str scoreForAbbreviation:abbr inRange:strRange fromRange:abbrRange hitMask:indexes];
-	XCTAssertEqualWithAccuracy(score, 0.90185, ACC);
-	results = [NSIndexSet indexSetFromArray:@[@(0), @(5), @(15), @(16), @(22), @(23)]];
-	XCTAssertEqualObjects(indexes, results);
-
-	strRange.length += STEP;
-	score = [str scoreForAbbreviation:abbr inRange:strRange fromRange:abbrRange hitMask:indexes];
-	XCTAssertEqualWithAccuracy(score, 0.90161, ACC);
-	results = [NSIndexSet indexSetFromArray:@[@(0), @(5), @(15), @(16), @(22), @(23), @(26)]];
-	XCTAssertEqualObjects(indexes, results);
-
-	strRange.length += STEP;
-	score = [str scoreForAbbreviation:abbr inRange:strRange fromRange:abbrRange hitMask:indexes];
-	XCTAssertEqualWithAccuracy(score, 0.90142, ACC);
-	results = [NSIndexSet indexSetFromArray:@[@(0), @(5), @(15), @(16), @(22), @(23), @(26)]];
-	XCTAssertEqualObjects(indexes, results);
-
-	strRange.length += STEP;
-	score = [str scoreForAbbreviation:abbr inRange:strRange fromRange:abbrRange hitMask:indexes];
-	XCTAssertEqualWithAccuracy(score, 0.74230, ACC);
-	results = [NSIndexSet indexSetFromArray:@[@(0), @(5), @(15), @(16), @(22), @(23), @(26), @(36)]];
-	XCTAssertEqualObjects(indexes, results);
-
-	strRange.length += STEP;
-	score = [str scoreForAbbreviation:abbr inRange:strRange fromRange:abbrRange hitMask:indexes];
-	XCTAssertEqualWithAccuracy(score, 0.69883, ACC);
-	results = [NSIndexSet indexSetFromArray:@[@(0), @(5), @(15), @(16), @(22), @(23), @(26), @(36), @(40), @(41)]];
-	XCTAssertEqualObjects(indexes, results);
-
-	strRange.length += STEP;
-	score = [str scoreForAbbreviation:abbr inRange:strRange fromRange:abbrRange hitMask:indexes];
-	XCTAssertEqualWithAccuracy(score, 0.71595, ACC);
-	results = [NSIndexSet indexSetFromArray:@[@(0), @(5), @(15), @(16), @(22), @(23), @(26), @(36), @(40), @(41)]];
-	XCTAssertEqualObjects(indexes, results);
-
-	strRange.length += STEP;
-	score = [str scoreForAbbreviation:abbr inRange:strRange fromRange:abbrRange hitMask:indexes];
-	XCTAssertEqualWithAccuracy(score, 0.73039, ACC);
-	results = [NSIndexSet indexSetFromArray:@[@(0), @(5), @(15), @(16), @(22), @(23), @(26), @(36), @(40), @(41)]];
-	XCTAssertEqualObjects(indexes, results);
-}
-
-- (void)testPerformanceNS {
-	[self measureBlock:^{
-		[@"Test string" scoreForAbbreviation:@"tsg"];
-	}];
-}
-- (void)testPerformanceCF {
+- (void)testPerformance {
     [self measureBlock:^{
 		CFStringRef str  = CFSTR("Test string");
 		CFStringRef abbr = CFSTR("tsg");
@@ -252,7 +137,7 @@ const float ACC = 0.00001;
 	}];
 }
 
-- (void)testPerformanceCFMicro {
+- (void)testPerformanceMicro {
 	[self measureBlock:^{
 		CFStringRef str  = CFSTR("This is a really long test string for testing");
 		CFStringRef abbr = CFSTR("tsg");
