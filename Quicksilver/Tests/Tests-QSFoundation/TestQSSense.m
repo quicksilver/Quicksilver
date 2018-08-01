ed14a46d253efde364546e46bcadb5d0e122aa37
Rob McBroom <github@skurfer.com>
2018-07-31 22:32:31 -0400

    adjust test scores after bug fixes

--- Quicksilver/Tests/Tests-QSFoundation/TestQSSense.m
+++ Quicksilver/Tests/Tests-QSFoundation/TestQSSense.m
@@ -57,25 +57,25 @@
 	NSIndexSet *results = nil;
 
 	score = QSScoreForAbbreviationWithRanges(str, CFSTR("test"), indexes, strRange, abbrRange);
-	XCTAssertEqualWithAccuracy(score, 0.74074, ACC);
+	XCTAssertEqualWithAccuracy(score, 0.901851, ACC);
 	results = [NSIndexSet indexSetFromArray:@[@(0), @(5), @(15), @(16), @(22), @(23)]];
 	XCTAssertEqualObjects(indexes, results);
 
 	strRange.length += STEP;
 	score = QSScoreForAbbreviationWithRanges(str, CFSTR("test"), indexes, strRange, abbrRange);
-	XCTAssertEqualWithAccuracy(score, 0.76129, ACC);
+	XCTAssertEqualWithAccuracy(score, 0.901612, ACC);
 	results = [NSIndexSet indexSetFromArray:@[@(0), @(5), @(15), @(16), @(22), @(23), @(26)]];
 	XCTAssertEqualObjects(indexes, results);
 
 	strRange.length += STEP;
 	score = QSScoreForAbbreviationWithRanges(str, CFSTR("test"), indexes, strRange, abbrRange);
-	XCTAssertEqualWithAccuracy(score, 0.77714, ACC);
+	XCTAssertEqualWithAccuracy(score, 0.901428, ACC);
 	results = [NSIndexSet indexSetFromArray:@[@(0), @(5), @(15), @(16), @(22), @(23), @(26)]];
 	XCTAssertEqualObjects(indexes, results);
 
 	strRange.length += STEP;
 	score = QSScoreForAbbreviationWithRanges(str, CFSTR("test"), indexes, strRange, abbrRange);
-	XCTAssertEqualWithAccuracy(score, 0.74230, ACC);
+	XCTAssertEqualWithAccuracy(score, 0.72948, ACC);
 	results = [NSIndexSet indexSetFromArray:@[@(0), @(5), @(15), @(16), @(22), @(23), @(26), @(36)]];
 	XCTAssertEqualObjects(indexes, results);
 
