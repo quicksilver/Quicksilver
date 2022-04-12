ba8e4bc8f174c7ee83215349f522722278f46046
Patrick Robertson <robertson.patrick@gmail.com>
2022-04-12 21:13:29 +0800

    Various fixes and improvements to link parser
    Try to decode data:image/XXX style image src
    Try to get the link name from the <a title=XXX ...></a> title attribute (if it exists)
    Fall back to getting images from data-src if we can't get them from src - useful for lazy-loaded images

--- Quicksilver/PlugIns-Main/QSCorePlugIn/Code/QSHTMLLinkParser.m
+++ Quicksilver/PlugIns-Main/QSCorePlugIn/Code/QSHTMLLinkParser.m
@@ -53,46 +53,86 @@
 	NSCharacterSet *wncs = [NSCharacterSet whitespaceAndNewlineCharacterSet];
 	for (HTMLElement *elem in [document nodesMatchingSelector:@"a"]) {
 		NSString *href = elem.attributes[@"href"];
-		if (!href || [href isEqualToString:@"#"]) {
+		if (!href || [href isEqualToString:@"#"] || [elem.attributes[@"aria-hidden"] isEqualToString:@"true"]) {
 			continue;
 		}
 		NSString *urlString = [[href stringByReplacingOccurrencesOfString:@"&amp; " withString:@"&"] stringByReplacingOccurrencesOfString:@"%s" withString:QUERY_KEY];
 		urlString = [[NSURL URLWithString:[urlString URLEncoding] relativeToURL:source] absoluteString];
-		
 		// skip over URLs that have already been found
 		if ([foundLinks containsObject:urlString]) {
 			continue;
 		}
 		[foundLinks addObject:urlString];
 		
-		NSString *name = [elem.textContent stringByTrimmingCharactersInSet:wncs];
+		NSString *name = [[[elem.textComponents arrayByEnumeratingArrayUsingBlock:^NSString *(NSString *string) {
+			return [string stringByTrimmingCharactersInSet:wncs];
+		}] componentsJoinedByString:@" "] stringByTrimmingCharactersInSet:wncs];
 		NSString *imageurl = nil;
 		HTMLElement *img = [elem firstNodeMatchingSelector:@"img"];
+		NSImage *loadedIcon = nil;
 		if (img && img.attributes[@"src"]) {
 			imageurl = img.attributes[@"src"];
-			if (!name || ![name length]) {
+			NSRange dataRange = [imageurl rangeOfString:@"data:"];
+			if (dataRange.location == 0) {
+				// img url starts with data: style image (e.g. data:image/png). Cut everything before the first comma
+				@try {
+					NSArray *parts = [imageurl componentsSeparatedByString:@","];
+					imageurl = [[parts subarrayWithRange:NSMakeRange(1, parts.count-1)] componentsJoinedByString:@","];
+					loadedIcon = [[NSImage alloc] initWithData:[imageurl dataUsingEncoding:NSUTF8StringEncoding]];
+					if (!loadedIcon) {
+						// couldn't convert data: string to image
+						// last attempt: try to get the icon from data-src (common with lazy-loaded images)
+						if (img.attributes[@"data-src"]) {
+							imageurl = img.attributes[@"data-src"];
+						} else {
+							// couldn't find any images :(
+							imageurl = nil;
+						}
+					}
+				} @catch (NSException *e){
+					// bail, it's a dodgy image src
+					imageurl = nil;
+				}
+			}
+			if (![name length]) {
                 // look for a link title here if none exists
-				for (NSString *attribute in @[@"title", @"alt", @"src"]) {
+				for (NSString *attribute in @[@"title", @"alt"]) {
 					if (img.attributes[attribute]) {
 						name = img.attributes[attribute];
-						if ([attribute isEqualToString:@"src"]) {
-							// take just the base part
-							name = [name lastPathComponent];
-						}
 						break;
 					}
 				}
 			}
 		}
 		name = [name stringByTrimmingCharactersInSet:wncs];
+		if (![name length] && elem.attributes[@"title"]) {
+			name = elem.attributes[@"title"];
+		}
+		if (![name length] && !img) {
+			// gets the text of e.g. buttons
+			name = [elem.textContent stringByTrimmingCharactersInSet:wncs];
+		}
+		
+		// still no name, but we have an image - use the last path component of the image url
+		imageurl = [[NSURL URLWithString:imageurl relativeToURL:source] absoluteString];
+		if (![name length] && imageurl) {
+			name = [imageurl lastPathComponent];
+		}
+		// if there's *still* no title (empty tag), skip it
+		if (![name length]) {
+			continue;
+		}
 		// make sure it's an actual URL
 		newObject = [QSObject URLObjectWithURL:urlString title:name];
 		[newObject assignURLTypesWithURL:urlString];
 		
 		[newObject setLabel:name];
-		// If the link is an image, set this as the icon
-		if (imageurl.length) {
-			imageurl = [[NSURL URLWithString:imageurl relativeToURL:source] absoluteString];
+		
+		if (loadedIcon) {
+			[newObject setIcon:loadedIcon];
+			[newObject setIconLoaded:YES];
+		} else if (imageurl.length) {
+			// load the image from the web
 			[newObject setObject:imageurl forMeta:kQSObjectIconName];
 			[newObject setIconLoaded:NO];
 		}
