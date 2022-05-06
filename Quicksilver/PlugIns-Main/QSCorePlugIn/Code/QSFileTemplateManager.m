e9f921cb07e6efa7431481ac4ca84af8e04e636b
Patrick Robertson <robertson.patrick@gmail.com>
2022-05-06 15:39:45 +0800

    Fix crash with 'Make New...' action
    If you accidentally enter text in the iObject for Folder → Make New... → [text], Quicksilver would crash. This avoids that crash

--- Quicksilver/PlugIns-Main/QSCorePlugIn/Code/QSFileTemplateManager.m
+++ Quicksilver/PlugIns-Main/QSCorePlugIn/Code/QSFileTemplateManager.m
@@ -12,16 +12,14 @@
 #import <QSCore/QSObject_FileHandling.h>
 
 @implementation QSFileTemplateManager
-#if 0
+
 - (QSObject *)instantiateTemplate:(QSObject *)dObject inDirectory:(QSObject *)iObject {
 	NSString *template = [dObject singleFilePath];
-	NSString *destination = [[[[iObject singleFilePath] stringByAppendingPathComponent:@"untitled"] stringByAppendingPathExtension:[template pathExtension]] firstUnusedFilePath];
-	[[NSFileManager defaultManager] copyItemAtPath:template toPath:destination error:nil];
-	return [QSObject fileObjectWithPath:destination];
-}
-#else
-- (QSObject *)instantiateTemplate:(QSObject *)dObject inDirectory:(QSObject *)iObject {
-	NSString *template = [dObject singleFilePath];
+	if (!template) {
+		// make sure that it's actually a file template
+		NSBeep();
+		return nil;
+	}
 	NSString *destination = [iObject singleFilePath];
 	destination = [[destination stringByAppendingPathComponent:@"untitled"] stringByAppendingPathExtension:
 		[template pathExtension]];
@@ -32,7 +30,6 @@
 	
 	return [QSObject fileObjectWithPath:destination];
 }
-#endif
 
 - (NSArray *)validIndirectObjectsForAction:(NSString *)action directObject:(QSObject *)dObject {
 	return [self templateObjects];
