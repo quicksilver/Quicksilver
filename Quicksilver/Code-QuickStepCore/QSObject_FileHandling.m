quicksilver/Quicksilver

commit 9245f0a43d74cc2a357dc3fe57c403f8430661bf

More efficiently get names of files
Reduce calls to expensive [QSObject infoRecord]

Quicksilver/Code-QuickStepCore/QSObject_FileHandling.m
@@ -901,29 +901,42 @@ NSArray *QSGetRecentDocumentsForBundle(NSString *bundleIdentifier) {
 
 - (void)getNameFromFiles {
 	NSString *newName = nil;
-	NSString *newLabel = nil;
 	if ([self count] >1) {
 		NSArray *paths = [self arrayForType:QSFilePathType];
 		NSString *container = [self filesContainer];
 		NSString *type = [self filesType];
 		BOOL onDesktop = [container isEqualToString:[@"~/Desktop/" stringByStandardizingPath]];
 		newName = [NSString stringWithFormat:@"%ld %@ %@ \"%@\"", (long)[paths count] , type, onDesktop?@"on":@"in", [container lastPathComponent]];
-	} else {
-		// generally: name = what you see in Terminal, label = what you see in Finder
-        newLabel = [[self infoRecord] objectForKey:NSURLLocalizedNameKey];
-        NSString *path = [self singleFilePath];
-        newName = [path lastPathComponent];
-        if (UTTypeConformsTo((__bridge CFStringRef)[self fileUTI], (CFStringRef)@"com.apple.systempreference.prefpane")) {
-            // kMDItemDisplayName works better for Preference Panes
-            MDItemRef mdItem = MDItemCreate(kCFAllocatorDefault, (__bridge CFStringRef)path);
-            if (mdItem) {
-                newLabel = (NSString *)CFBridgingRelease(MDItemCopyAttribute(mdItem, kMDItemDisplayName));
-                CFRelease(mdItem);
-            }
-        }
+		[self setName:newName];
+		return;
 	}
-    [self setName:newName];
-	[self setLabel:newLabel];
+	// single object
+	
+	NSString *path = [self singleFilePath];
+	newName = [path lastPathComponent];
+	
+	if ([[path pathExtension] isEqualToString:@"app"]) {
+		// most apps just remove the extension
+		newName = [path stringByDeletingPathExtension];
+	}
+	[self setName:newName];
+	[self setLabel:newName];
+	
+	// generally: name = what you see in Terminal, label = what you see in Finder
+	
+	NSString *newLabel = [[self infoRecord] objectForKey:NSURLLocalizedNameKey];
+	if (UTTypeConformsTo((__bridge CFStringRef)[self fileUTI], (CFStringRef)@"com.apple.systempreference.prefpane")) {
+		// kMDItemDisplayName works better for Preference Panes
+		MDItemRef mdItem = MDItemCreate(kCFAllocatorDefault, (__bridge CFStringRef)path);
+		if (mdItem) {
+			newLabel = (NSString *)CFBridgingRelease(MDItemCopyAttribute(mdItem, kMDItemDisplayName));
+			CFRelease(mdItem);
+		}
+	}
+	if (![newLabel isEqualToString:newName]) {
+		[self setLabel:newLabel];
+	}
+
 }
 
 - (NSString *)filesContainer {

commit 81e3c5ce597e0ef81e815e1368e57c80e416310e

Fix file naming issues introduced in 9245f0a43d74cc2a357dc3fe57c403f8430661bf

Quicksilver/Code-QuickStepCore/QSObject_FileHandling.m
@@ -891,20 +891,18 @@ NSArray *QSGetRecentDocumentsForBundle(NSString *bundleIdentifier) {
 		return;
 	}
 	// single object
-	
 	NSString *path = [self singleFilePath];
 	newName = [path lastPathComponent];
-	
-	if ([[path pathExtension] isEqualToString:@"app"]) {
-		// most apps just remove the extension
-		newName = [path stringByDeletingPathExtension];
-	}
+
 	[self setName:newName];
-	[self setLabel:newName];
 	
 	// generally: name = what you see in Terminal, label = what you see in Finder
 	
 	NSString *newLabel = [[self infoRecord] objectForKey:NSURLLocalizedNameKey];
+	if (UTTypeConformsTo((__bridge CFStringRef)[self fileUTI], (CFStringRef)@"com.apple.systempreference.prefpane")) {
+		// get the CFBundleName for preference panes
+		newLabel = [[NSBundle bundleWithPath:path] localizedInfoDictionary][@"CFBundleName"];
+	}
 	if (![newLabel isEqualToString:newName]) {
 		[self setLabel:newLabel];
 	}