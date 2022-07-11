3f605d4731388152747ec3de951d289466fb9ce1
Patrick Robertson <robertson.patrick@gmail.com>
2022-07-11 23:11:48 +0800

    Unit tests for NDKeyboardLayout

--- Quicksilver/Tests/Tests-QSFoundation/TestNDHotkey.m
+++ Quicksilver/Tests/Tests-QSFoundation/TestNDHotkey.m
@@ -21,16 +21,9 @@
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testCorrectKeyStrings {
+    NSString *str = [[NDKeyboardLayout keyboardLayout] stringForKeyCode:124 modifierFlags:11534600];
+    XCTAssertEqualObjects(str, @"⌘→");
 }
 
 @end
