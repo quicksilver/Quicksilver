16e38e07dc6b93b890861b2edd3f0ee7f7a6aa4c
Nathan Henrie <nate@n8henrie.com>
2022-10-23 16:48:04 -0600

    Expand special handling to applications as well as system prefs

--- Quicksilver/Code-QuickStepCore/QSObject_FileHandling.m
+++ Quicksilver/Code-QuickStepCore/QSObject_FileHandling.m
@@ -899,14 +899,17 @@ NSArray *QSGetRecentDocumentsForBundle(NSString *bundleIdentifier) {
 	// generally: name = what you see in Terminal, label = what you see in Finder
 	
 	NSString *newLabel = [[self infoRecord] objectForKey:NSURLLocalizedNameKey];
-	if (UTTypeConformsTo((__bridge CFStringRef)[self fileUTI], (CFStringRef)@"com.apple.systempreference.prefpane")) {
-		// get the CFBundleName for preference panes
-		newLabel = [[NSBundle bundleWithPath:path] localizedInfoDictionary][@"CFBundleName"];
+
+	NSArray* types = @[@"com.apple.systempreference.prefpane", @"com.apple.application-bundle"];
+	for (NSString* type in types) {
+		if (UTTypeConformsTo((__bridge CFStringRef)[self fileUTI], (__bridge CFStringRef _Nonnull)(type))) {
+			// get the CFBundleName for preference panes
+			newLabel = [[NSBundle bundleWithPath:path] localizedInfoDictionary][@"CFBundleName"];
+		}
 	}
 	if (![newLabel isEqualToString:newName]) {
 		[self setLabel:newLabel];
 	}
-
 }
 
 - (NSString *)filesContainer {
