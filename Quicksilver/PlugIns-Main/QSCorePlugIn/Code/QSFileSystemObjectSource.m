quicksilver/Quicksilver

commit 58741f77931591d7299923cbe64c0e1ea76dfe77

Watch all directories down to the 'depth' level (#2649)
Previously, if you have a Catalog entry with depth of 3, the file/folder watching would only work on the 'top' folder level and lower level folders wouldn't be watched. This change makes QS watch folders down to the 'depth' specified in the catalog entry's settings. Fixes #2509

Quicksilver/PlugIns-Main/QSCorePlugIn/Code/QSFileSystemObjectSource.m
@@ -11,6 +11,7 @@
 #import "QSVoyeur.h"
 
 #import "QSObject_FileHandling.h"
+#import "QSDirectoryParser.h"
 
 #import "QSUTI.h"
 
@@ -328,14 +329,16 @@
 
 - (void)enableWatching {
 	NSMutableDictionary *settings = self.sourceSettings;
-	NSString *path = self.fullWatchPath;
+	NSArray *paths = self.fullWatchPaths;
 	NSNotificationCenter *wsNotif = [[NSWorkspace sharedWorkspace] notificationCenter];
+	for (NSString *path in paths) {
 	[[QSVoyeur sharedInstance] addPath:path notifyingAbout:NOTE_DELETE | NOTE_WRITE];
 #ifdef DEBUG
 	if (VERBOSE) NSLog(@"Watching Path %@", path);
 #endif
 	[wsNotif addObserver:self selector:@selector(invalidateIndex:) name:nil object:path];
-	NSArray *paths = settings[kQSWatchPaths];
+	}
+	paths = settings[kQSWatchPaths];
 	for (NSString * p in paths) {
 		[[QSVoyeur sharedInstance] addPath:p];
 #ifdef DEBIG
@@ -347,10 +350,13 @@
 
 - (void)disableWatching {
 	#ifdef DEBUG
-		if (VERBOSE) NSLog(@"Disable watching path %@", self.fullWatchPath);
+		if (VERBOSE) NSLog(@"Disable watching paths %@", self.fullWatchPaths);
 	#endif
+	NSArray *paths = self.fullWatchPaths;
+	for (NSString *path in paths) {
+		[[QSVoyeur sharedInstance] removePath:path];
+	}
 	NSNotificationCenter *wsNotif = [[NSWorkspace sharedWorkspace] notificationCenter];
-	[[QSVoyeur sharedInstance] removePath:self.fullWatchPath];
 	[wsNotif removeObserver:self];
 	for (NSString *p in self.sourceSettings[kQSWatchPaths]) {
 		[[QSVoyeur sharedInstance] removePath:p];
@@ -368,6 +374,18 @@
 		itemPath = [bundlePath stringByAppendingPathComponent:itemPath];
 	}
 	return itemPath;
+}
+
+- (NSArray *)fullWatchPaths {
+	NSDictionary *settings = self.sourceSettings;
+	NSString *itemPath = [self fullWatchPath];
+	NSInteger depth = [(NSNumber *)[settings objectForKey:kItemFolderDepth] integerValue];
+	if (depth == 1) {
+		return @[itemPath];
+	}
+    QSDirectoryParser *parser = [QSDirectoryParser new];
+	NSArray *files = [[parser objectsFromPath:itemPath depth:depth types:@[@"public.folder"] excludeTypes:nil descend:NO] arrayByPerformingSelector:@selector(singleFilePath)];
+	return files;
 }
 
 @end

Quicksilver/PlugIns-Main/QSCorePlugIn/Code/QSFileSystemObjectSource.h
@@ -23,7 +23,6 @@
 - (IBAction)chooseFile:(id)sender;
 - (BOOL)chooseFile;
 
-- (NSString *)fullPathForSettings:(NSDictionary *)settings;
 - (IBAction)endContainingSheet:(id)sender;
 
 @end
@@ -34,4 +33,5 @@
 -(void)enableWatching;
 -(void)disableWatching;
 @property (readonly) NSString *fullWatchPath;
+@property (readonly) NSArray *fullWatchPaths;
 @end

commit 64344eeab6e95f88d464433fc5c77d8aa62ddf36

Fix bug introduced by 58741f7 - when watching folders, also watch the root (top) folder
My previous code changes meant only the sub-folders were watched, and not the main folder. This is now fixed here

Quicksilver/PlugIns-Main/QSCorePlugIn/Code/QSFileSystemObjectSource.m
@@ -332,9 +332,9 @@
 	NSArray *paths = self.fullWatchPaths;
 	NSNotificationCenter *wsNotif = [[NSWorkspace sharedWorkspace] notificationCenter];
 	for (NSString *path in paths) {
-	[[QSVoyeur sharedInstance] addPath:path notifyingAbout:NOTE_DELETE | NOTE_WRITE];
+	[[QSVoyeur sharedInstance] addPath:path notifyingAbout:NOTE_DELETE | NOTE_WRITE | NOTE_RENAME];
 #ifdef DEBUG
-	if (VERBOSE) NSLog(@"Watching Path %@", path);
+	 NSLog(@"Watching Path %@", path);
 #endif
 	[wsNotif addObserver:self selector:@selector(invalidateIndex:) name:nil object:path];
 	}
@@ -342,9 +342,12 @@
 	for (NSString * p in paths) {
 		[[QSVoyeur sharedInstance] addPath:p];
 #ifdef DEBIG
-		if (VERBOSE) NSLog(@"Watching Path %@", p);
+		NSLog(@"Watching Path %@", p);
 #endif
 		[wsNotif addObserver:self selector:@selector(invalidateIndex:) name:VDKQueueWriteNotification object:p];
+        [wsNotif addObserver:self selector:@selector(invalidateIndex:) name:VDKQueueDeleteNotification object:p];
+        [wsNotif addObserver:self selector:@selector(invalidateIndex:) name:VDKQueueRenameNotification object:p];
+
 	}
 }
 
@@ -361,6 +364,9 @@
 	for (NSString *p in self.sourceSettings[kQSWatchPaths]) {
 		[[QSVoyeur sharedInstance] removePath:p];
 		[wsNotif removeObserver:self name:VDKQueueWriteNotification object:p];
+        [wsNotif removeObserver:self name:VDKQueueDeleteNotification object:p];
+        [wsNotif removeObserver:self name:VDKQueueRenameNotification object:p];
+
 	}
 }
 
@@ -383,8 +389,10 @@
 	if (depth == 1) {
 		return @[itemPath];
 	}
+    NSArray *files = @[itemPath];
     QSDirectoryParser *parser = [QSDirectoryParser new];
-	NSArray *files = [[parser objectsFromPath:itemPath depth:depth types:@[@"public.folder"] excludeTypes:nil descend:NO] arrayByPerformingSelector:@selector(singleFilePath)];
+    NSArray *descendentFiles = [parser objectsFromPath:itemPath depth:depth types:@[@"public.folder"] excludeTypes:nil descend:NO];
+    files = [files arrayByAddingObjectsFromArray:[descendentFiles arrayByPerformingSelector:@selector(singleFilePath)]];
 	return files;
 }
 