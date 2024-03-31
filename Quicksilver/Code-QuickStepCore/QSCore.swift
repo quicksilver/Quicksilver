85ec098b4592506c8980f4cf7437c162368401d4
Nathan Henrie <nate@n8henrie.com>
2024-03-30 18:24:06 -0600

    Add example swift file

---
+++ Quicksilver/Code-QuickStepCore/QSCore.swift
@@ -0,0 +1,14 @@
+//
+//  QSCore.swift
+//  QuickStep Core
+//
+//  Created by Nathan Henrie on 2024-03-30.
+//
+
+import Foundation
+
+@objc public class QSPMS: NSObject {
+    override init() {
+        print("Hello from swift")
+    }
+}
