2f5f60532b482af7f06bfd7f33a0b4fe031fd680
Patrick Robertson <robertson.patrick@gmail.com>
2025-04-09 18:26:44 +0200

    Tweaks for CI

---
+++ Quicksilver/Tools/codesign/import_certificate_into_new_keychain.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -exu
+
+certificateFile="$1"
+certificatePassword="$2"
+
+keychain="alt-tab-macos.keychain"
+keychainPassword="password"
+
+# create a keychain
+security create-keychain -p $keychainPassword $keychain
+# make keychain default so xcodebuild uses it
+security default-keychain -s $keychain
+# unlock keychain
+security unlock-keychain -p $keychainPassword $keychain
+# import p12 into Keychain
+security import $certificateFile.p12 -P "$certificatePassword" -T /usr/bin/codesign
+security set-key-partition-list -S apple-tool:,apple: -s -k $keychainPassword $keychain > /dev/null
