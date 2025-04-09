5ee9fd94518eb00664748701f50aa097d2ff8eb2
Patrick Robertson <robertson.patrick@gmail.com>
2025-04-09 18:19:57 +0200

    Fix codesigning for CI

--- Quicksilver/Tools/codesign/setup_local.sh
+++ Quicksilver/Tools/codesign/setup_local.sh
@@ -5,5 +5,10 @@ set -exu
 certificateFile="codesign"
 certificatePassword=$(openssl rand -base64 12)
 
-Quicksilver/Tools/codesign/generate_selfsigned_certificate.sh "$certificateFile" "$certificatePassword"
-Quicksilver/Tools/codesign/import_certificate_into_main_keychain.sh "$certificateFile" "$certificatePassword"
+# cd to Quicksilver directory if 'Tools' is not a subdirectory of the current directory
+if [[ ! -d "Tools" ]]; then
+  cd "Quicksilver"
+fi
+
+./Tools/codesign/generate_selfsigned_certificate.sh "$certificateFile" "$certificatePassword"
+./Tools/codesign/import_certificate_into_main_keychain.sh "$certificateFile" "$certificatePassword"
